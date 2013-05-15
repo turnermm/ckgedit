94fbeefef87ac80c0bddc5b95bb61d2082477d57
Myron Turner <turnermm02@shaw.ca>
2013-05-14 19:51:49 -0500

    Additional error checking for fckeditor file uploads

--- ckgedit/fckeditor/editor/filemanager/connectors/php/commands.php
+++ ckgedit/fckeditor/editor/filemanager/connectors/php/commands.php
@@ -592,7 +592,11 @@ function FileUpload( $resourceType, $currentFolder, $sCommand )
 	{
 		global $Config ;
 
- 
+        $upload_err = $_FILES['NewFile']['error'];
+        if($upload_err ) {
+            send_ckg_UploadError($upload_err,$sFileUrl, $_FILES['NewFile']['name']);           
+            exit;
+        }
 		$oFile = $_FILES['NewFile'] ;
 
 
@@ -741,6 +745,45 @@ function mkdir_rek($dir, $mode = 0777)
 	}
 }
 
+function send_ckg_UploadError($err,$sFileUrl, $file) {
+    switch($err) {
+        
+        case UPLOAD_ERR_INI_SIZE:
+            $msg = "The uploaded file exceeds the upload_max_filesize directive in php.ini.";
+            break;
+            
+        case UPLOAD_ERR_FORM_SIZE:
+        $msg = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.";
+        break;
+        
+        case UPLOAD_ERR_PARTIAL:
+            $msg = "The uploaded file was only partially uploaded.";
+            break;
+            
+        case UPLOAD_ERR_NO_FILE:
+            $msg = "No file was uploaded.";
+            break;
+
+       case 6:       //   UPLOAD_ERR_NO_TMP_DIR  Introduced in PHP 4.3.10 and PHP 5.0.3.
+            $msg = "Missing a temporary folder.";
+            break;
+        
+       case 7:  //UPLOAD_ERR_CANT_WRITE  Introduced in PHP 5.1.0.
+            $msg="Failed to write file to disk.";
+            break;
+       
+        case 8: //UPLOAD_ERR_EXTENSION   Introduced in PHP 5.2.0. 
+            $msg="PHP does not provide a way to ascertain which extension caused the file upload to stop; examining the list of loaded extensions with phpinfo() may help.";
+            break;
+           
+        default:
+           $msg = "Undetermined  upload error";
+           break;
+    }     
+      $upload_error = 300 + $err;
+       SendUploadResults( $upload_error, $sFileUrl, $file,  $msg ) ;
+      
+}   
 
 function cmd_write_debug($what) {
 return;
