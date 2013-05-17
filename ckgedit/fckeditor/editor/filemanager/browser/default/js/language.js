6e28a1af6c1b27bb7eefb4e58c35499a59e6cd01
Myron Turner <turnermm02@shaw.ca>
2013-05-16 19:39:59 -0500

    added language support to filebrowser error messages

--- ckgedit/fckeditor/editor/filemanager/browser/default/js/language.js
+++ ckgedit/fckeditor/editor/filemanager/browser/default/js/language.js
@@ -1,31 +1,27 @@
 var fck_Lang;
 
-function setupLanguage(file) {
+function setupLanguage() {
 
-return;
- if(opener && opener.FCK.Language) {
-    fck_Lang=opener.FCKLang;
-    opener.FCK.Language.TranslatePage(document);
-    return;  
+     if(opener && opener.oDokuWiki_FCKEditorInstance.Lang) {
+        if(opener.oDokuWiki_FCKEditorInstance.Lang['fbrowser'])
+            fck_Lang=opener.oDokuWiki_FCKEditorInstance.Lang['fbrowser'];               
+            return;  
  }
- /*
- if (parent && parent.opener.FCK.Language) {
-    fck_Lang=parent.opener.FCKLang;
-    parent.opener.FCK.Language.TranslatePage(document);
-    return;  
 
+     if (parent && parent.opener.oDokuWiki_FCKEditorInstance.Lang) {
+        fck_Lang=parent.opener.oDokuWiki_FCKEditorInstance.Lang['fbrowser'];          
+        return;  
  }
  
-*/
-
 }
 
 function translateItem(js_code, default_str) {
-/*
+
+  if(!fck_Lang) return default_str;
   if(fck_Lang[js_code] && fck_Lang[js_code] != "") {
         return fck_Lang[js_code];
   }
-  */
+  
   return default_str;
 
 }
@@ -33,7 +29,7 @@ function translateItem(js_code, default_str) {
 
 /* translate errors for error case 1 which implements variable error messages */
 function translateErrorAny(err_str) {
-return err_str;
+ if(!fck_Lang) return err_str;
  if(!err_str.match(/^FileBrowserError_/)) {
      return err_str;    
  }
