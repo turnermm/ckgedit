533c55e763c3edc93ed8711f50c06b0690650152
Myron Turner <turnermm02@shaw.ca>
2013-05-25 09:08:57 -0500

    converted sack calls to jQuery

--- ckgedit/ckeditor/config.js
+++ ckgedit/ckeditor/config.js
@@ -73,199 +73,11 @@ config.colors = new Array(
 'FFFFFF','FFFFFF','FFFFFF'
 );
 
-
-
-function sack(file) {
-	this.xmlhttp = null;
-
-	this.resetData = function() {
-		this.method = "POST";
-  		this.queryStringSeparator = "?";
-		this.argumentSeparator = "&";
-		this.URLString = "";
-		this.encodeURIString = true;
-  		this.execute = false;
-  		this.element = null;
-		this.elementObj = null;
-		this.requestFile = file;
-		this.vars = new Object();
-		this.responseStatus = new Array(2);
-  	};
-
-	this.resetFunctions = function() {
-  		this.onLoading = function() { };
-  		this.onLoaded = function() { };
-  		this.onInteractive = function() { };
-  		this.onCompletion = function() { };
-  		this.onError = function() { };
-		this.onFail = function() { };
-	};
-
-	this.reset = function() {
-		this.resetFunctions();
-		this.resetData();
-	};
-
-	this.createAJAX = function() {
-		try {
-			this.xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
-		} catch (e1) {
-			try {
-				this.xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-			} catch (e2) {
-				this.xmlhttp = null;
-			}
-		}
-
-		if (! this.xmlhttp) {
-			if (typeof XMLHttpRequest != "undefined") {
-				this.xmlhttp = new XMLHttpRequest();
-			} else {
-				this.failed = true;
-			}
-		}
-	};
-
-	this.setVar = function(name, value){
-		this.vars[name] = Array(value, false);
-	};
-
-	this.encVar = function(name, value, returnvars) {
-		if (true == returnvars) {
-			return Array(encodeURIComponent(name), encodeURIComponent(value));
-		} else {
-			this.vars[encodeURIComponent(name)] = Array(encodeURIComponent(value), true);
-		}
-	}
-
-	this.processURLString = function(string, encode) {
-		encoded = encodeURIComponent(this.argumentSeparator);
-		regexp = new RegExp(this.argumentSeparator + "|" + encoded);
-		varArray = string.split(regexp);
-		for (i = 0; i < varArray.length; i++){
-			urlVars = varArray[i].split("=");
-			if (true == encode){
-				this.encVar(urlVars[0], urlVars[1]);
-			} else {
-				this.setVar(urlVars[0], urlVars[1]);
-			}
-		}
-	}
-
-	this.createURLString = function(urlstring) {
-		if (this.encodeURIString && this.URLString.length) {
-			this.processURLString(this.URLString, true);
-		}
-
-		if (urlstring) {
-			if (this.URLString.length) {
-				this.URLString += this.argumentSeparator + urlstring;
-			} else {
-				this.URLString = urlstring;
-			}
-		}
-
-		// prevents caching of URLString
-		this.setVar("rndval", new Date().getTime());
-
-		urlstringtemp = new Array();
-		for (key in this.vars) {
-			if (false == this.vars[key][1] && true == this.encodeURIString) {
-				encoded = this.encVar(key, this.vars[key][0], true);
-				delete this.vars[key];
-				this.vars[encoded[0]] = Array(encoded[1], true);
-				key = encoded[0];
-			}
-
-			urlstringtemp[urlstringtemp.length] = key + "=" + this.vars[key][0];
-		}
-		if (urlstring){
-			this.URLString += this.argumentSeparator + urlstringtemp.join(this.argumentSeparator);
-		} else {
-			this.URLString += urlstringtemp.join(this.argumentSeparator);
-		}
-	}
-
-	this.runResponse = function() {
-		eval(this.response);
-	}
-
-	this.runAJAX = function(urlstring) {
-		if (this.failed) {
-			this.onFail();
-		} else {
-			this.createURLString(urlstring);
-			if (this.element) {
-				this.elementObj = document.getElementById(this.element);
-			}
-			if (this.xmlhttp) {
-				var self = this;
-				if (this.method == "GET") {
-					totalurlstring = this.requestFile + this.queryStringSeparator + this.URLString;
-					this.xmlhttp.open(this.method, totalurlstring, true);
-				} else {
-					this.xmlhttp.open(this.method, this.requestFile, true);
-					try {
-						this.xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-					} catch (e) { }
-				}
-
-				this.xmlhttp.onreadystatechange = function() {
-					switch (self.xmlhttp.readyState) {
-						case 1:
-							self.onLoading();
-							break;
-						case 2:
-							self.onLoaded();
-							break;
-						case 3:
-							self.onInteractive();
-							break;
-						case 4:
-							self.response = self.xmlhttp.responseText;
-							self.responseXML = self.xmlhttp.responseXML;
-							self.responseStatus[0] = self.xmlhttp.status;
-							self.responseStatus[1] = self.xmlhttp.statusText;
-
-							if (self.execute) {
-								self.runResponse();
-							}
-
-							if (self.elementObj) {
-								elemNodeName = self.elementObj.nodeName;
-								elemNodeName.toLowerCase();
-								if (elemNodeName == "input"
-								|| elemNodeName == "select"
-								|| elemNodeName == "option"
-								|| elemNodeName == "textarea") {
-									self.elementObj.value = self.response;
-								} else {
-									self.elementObj.innerHTML = self.response;
-								}
-							}
-							if (self.responseStatus[0] == "200") {
-								self.onCompletion();
-							} else {
-								self.onError();
-							}
-
-							self.URLString = "";
-							break;
-					}
-				};
-
-				this.xmlhttp.send(this.URLString);
-			}
-		}
-	};
-
-	this.reset();
-	this.createAJAX();
-}
      
       var ckedit_path =  window.location.protocol +'//' + top.dokuBase + 'lib/plugins/ckgedit/ckeditor/';
       config.doku_url = window.location.protocol+ '//' + top.dokuBase;
       config.ckedit_path = window.location.protocol +'//' + top.dokuBase + 'lib/plugins/ckgedit/ckeditor/';
+      config.jquery = top.jQuery;
     //  config.format_code = { element : 'code', attributes : { 'class' : 'dwcode' } };
 	// Define changes to default configuration here.
 	// For the complete reference:
@@ -335,61 +147,56 @@ config.dokuSmileyImages	=
 
 
                           
-function do_smileys(){
-	ajax.requestFile =  ckedit_path + "dwsmileys.php";
-	ajax.method = 'POST';
-	ajax.onCompletion = whenCompleted;
-	ajax.runAJAX();
+
+function do_smileys() {
+    config.jquery.post(
+            ckedit_path + "dwsmileys.php",                
+            function(data,status) {    
+                if(status == "success") {
+                   config.dokuSmileyConfImages = new Array();
+                   smileys = data.replace(/#.*?\n/g,"");
+                   smileys = smileys.replace(/^[\s\n]+$/mg,"");
+                   smileys=smileys.split(/\n/);
+                   if(!smileys[0]) smileys.shift();
+                   if(!smileys[smileys.length-1]) smileys.pop();   
+                   for(var i=0; i < smileys.length; i++) {            
+                        var a = smileys[i].split(/\s+/);
+                        if(a[0].match(/DELETEME/) || a[0].match(/FIXME/)) { 
+                           config.dokuFixmeSmiley.push( a);
+                           continue;
+                        }
+                         config.dokuSmileyConfImages[i] = a;
+                  }      
+                }
+             },
+            'html'
+        );
 }
 
 config.dokuFixmeSmiley = new Array();
-
-function whenCompleted(){
-    
-    if(ajax.responseStatus && ajax.responseStatus[0] == 200) {
-
-       config.dokuSmileyConfImages = new Array();
-       smileys = ajax.response.replace(/#.*?\n/g,"");
-       smileys = smileys.replace(/^[\s\n]+$/mg,"");
-       smileys=smileys.split(/\n/);
-       if(!smileys[0]) smileys.shift();
-       if(!smileys[smileys.length-1]) smileys.pop();   
-       for(var i=0; i < smileys.length; i++) {            
-            var a = smileys[i].split(/\s+/);
-            if(a[0].match(/DELETEME/) || a[0].match(/FIXME/)) { 
-               config.dokuFixmeSmiley.push( a);
-               continue;
-            }
-             config.dokuSmileyConfImages[i] = a;
-      }      
-    }
-}
-
-
 config.dokuSmileyConfImages;
 try {
-var ajax = new sack();
    do_smileys();
 }catch(ex){
 
 }
-config.sack =sack;
+
 config.ckgEditorVer;
 var get_ckgeditor_version = function() {
-    var ajax = new sack();  
-	ajax.requestFile =  ckedit_path + "get_version.php";
-	ajax.method = 'POST';
-	ajax.onCompletion = function() {
-	    if(ajax.responseStatus && ajax.responseStatus[0] == 200) {
-		   config.ckgEditorVer=ajax.response;		   
-        }
-	};
-	ajax.runAJAX();
-
+    config.jquery.post(
+            ckedit_path + "get_version.php",
+            function (data,status) { 
+                if(status == "success") {            
+                    config.ckgEditorVer=data;
+                }
+                else config.ckgEditorVer = "Dokuwiki plugin: ckgedit";               
+            },
+            'html'
+        );
 }
 
 get_ckgeditor_version();   
-	      
+
 //	config.removeButtons = 'Underline,Subscript,Superscript,Anchor';
 // This is actually the default value.
 config.toolbar_Full =
