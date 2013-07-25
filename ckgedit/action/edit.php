18f44faf165bf154afdf43824d0248c3eb9dc33b
Myron Turner <turnermm02@shaw.ca>
2013-07-25 14:05:39 -0500

    corrects missing final  monospace markup in list items

--- ckgedit/action/edit.php
+++ ckgedit/action/edit.php
@@ -1997,7 +1997,7 @@ function parse_wikitext(id) {
         return; 
      }
 
-     if(tag == 'code') {     // empty code markup corrupts results
+     if(tag == 'code' && ! this.list_started) {     // empty code markup corrupts results    
            if(results.match(/''\s*$/m)) {                     
              results = results.replace(/''\s*$/, "\n");                
              return;
