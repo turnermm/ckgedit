f389d99df227bf8ac5f0460b773cffc87d7b8e95
Robert Bogenschneider <bogi@uea.org>
2014-02-09 16:50:42 +0100

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['groups']                = 'Grupo, kiu rajtas malebligi la blokad-kalkulilon (malaktuala)';
+$lang['middot']                = 'Komo-disigita listo de grupoj kiuj uzas &amp;middot; por &amp;nbsp;';
+$lang['big_files']             = 'Ĉu sekure modifi tro grandajn dosierojn?';
+$lang['big_file_sz']           = 'Limo de dosiergrandeco (bitokoj)';
+$lang['big_file_tm']           = 'Prilaborado de tro grandaj dosieroj ĉesas post tiom da sekundoj:';
+$lang['fck_preview']           = 'FCK Antaŭrigarda grupo';
+$lang['guest_toolbar']         = 'Montri la ilaron al gastoj';
+$lang['guest_media']           = 'Gastoj rajtas ligi al aŭdvidaj dosieroj';
+$lang['open_upload']           = 'Gastoj rajtas alŝuti';
+$lang['default_fb']            = 'Maniero de dosierrigardado. Sen tio ACL ne validas (ne okazas alirkontrolo).';
+$lang['openfb']                = 'Malferma rigardado de dosieroj. Tio ebligas uzantojn aliri kompletajn dosierujajn strukturojn, sendepende de legpermesoj. Alirkontrolo tamen validas por alŝutoj.';
+$lang['dw_edit_display']       = 'Difinas, kiuj uzantoj povas aliri la "DW Edit"-butonon. Elektoj: "all" por ĉiuj uzantoj; "admin" nur por la superuzanto kaj administrantoj; "none" por neniu. Standardo estas "all".';
+$lang['smiley_as_text']        = 'Montri ridetulojn kiel tekston en CKeditor (montriĝos tamen kiel bildo en foliumilo)';
+$lang['editor_bak']            = 'Konservi sekureckopion al meta/&lt;namespace&gt;.ckgedit';
+$lang['create_folder']         = 'Ĉu ebligi butonon por krei dosierujojn en la dosier-foliumilo (y/n)?';
+$lang['dwedit_ns']             = 'Komo-disigita listo de nomspacoj, kie ckgedit aŭtomate ŝaltas al la interna DokuWiki-editoro.';
+$lang['acl_del']               = 'La defaŭlto (sen marko) permesas al uzantoj kun alŝutpermeso forigi aŭdvidajn dosierojn; se la kesto estas markita, la uzanto bezonas forig-permeson por forigi ion el la dosierujo.';
+$lang['auth_ci']               = 'Ĉu ignori usklecon, tio signifas, ke vi povas ensaluti kaj kiel UZANTO kaj kiel uzanto?';
+$lang['nix_style']             = 'Por Vindozaj serviloj (Vista kaj poste). Tiu agordo ebligas aliri data\media tra ckgedit\CKeditor\userfiles, se ligiloj al dosieroj sukcese kreiĝis en userfiles';
+$lang['no_symlinks']           = 'Malebligi aŭtomatan kreadon de simbolaj ligiloj en ckgedit/userfiles.';
+$lang['direction']             = 'Indiki la direkton de la lingvo en CKeditor: <b>nocheck</b>: ckgedit faras neniujn ŝanĝojn en la standarda lingvodirekto; <b>dokuwiki</b>: la momenta lingvodirekto de Dokuwiki; <b>ltr</b>: Maldekstre-dekstren; <b>rtl</b>: Dekstre-maldekstren.';
+$lang['scayt_auto']            = 'Aŭtomate aktivigi la SCAYT-korektilon. Standardo estas "on". Por malŝalti SCAYT, elektu "off".';
+$lang['scayt_lang']            = 'Indiki la standardan lingvon por SCAYT.';
+$lang['smiley_hack']           = 'Nuligi URL por la ridetuloj de CKEditor dum translokiĝo al alia servilo. Ĉio okazas paĝon post paĝo, kiam paĝo estas ŝargata por modifado kaj konservata. Normale la opcio estu malŝaltita.';
+$lang['complex_tables']        = 'Uzi la algoritmon por kompleksaj tabeloj. Male al la standarda parsado se tabeloj, devus veni pli bonaj rezultoj kun kompleksaj aranĝoj de kombinitaj linioj kaj kolumnoj. Sed bezonas iom pli da prilabora tempo.';
+$lang['duplicate_notes']       = 'Diru jes al tio, se uzantoj kreas plurajn piednotojn el la sama piednota teksto; necesa por eviti fuŝadon de piednotoj.';
+$lang['winstyle']              = 'Uzi rektan padon al la aŭdvida dosierujo anstataŭ fckeditor/userfiles. Tio necesigas kopii fckeditor/userfiles/.htaccess.security al data/media kaj renomi ĝin .htaccess';
+$lang['other_lang']            = 'La standarda lingvo de la CKEditoro estas la lingvo instalita en via foliumilo. Vi povas tamen elekti alian lingvon ĉi tie; temas pri interfaca lingvo sendependa de DokuWiki.';
+$lang['dw_priority']           = 'Uzi la DokuWiki-editoron standarde';
