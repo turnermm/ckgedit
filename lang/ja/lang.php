9ac02d7e0b24689185f7cdde47508235f70d4da8
Nobuyuki Fukuyori <self@spumoni.org>
2014-10-21 06:25:35 +0200

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -1,29 +1,30 @@
 <?php
-//global $lang;
 
-$lang['btn_dw_edit'] = "テキストモードに変更";
-$lang['dw_btn_fck_preview']="プレビュー";
-$lang['dw_btn_lang']="言語を選択";
-$lang['title_dw_delete'] = "ページを削除";
-$lang['title_dw_edit'] = "内容を保存して編集を終わり、DokuWiki組み込みのエディタに切り替え";
-$lang['dw_btn_revert'] = "戻す";
-$lang['title_dw_revert'] = "以前のドラフトに戻す";
-$lang['title_dw_lang']="スペルチェッカーの言語を選択";
-$lang['title_dw_cancel']='編集をキャンセル';
-$lang['btn_fck_edit'] = "GUIモードに変更";
-$lang['confirm_delete'] = "本当にこのページを削除してもよろしいですか？";
-$lang['confirm_preview'] = "編集中の内容は保存されず失われます";
-
-$lang['dw_btn_backup'] ="ドラフトを保存";
-$lang['title_dw_backup'] ="内容をドラフトに保存して、ロックの期限を更新";
-$lang['backup_empty'] = "以前のドラフトは存在しません。このまま戻すと編集中の内容はすべて失われますがよろしいですか？";
-$lang['btn_draft'] = "ドラフトを表示";
-$lang['btn_exit_draft'] = "現在の文書に戻る";
-$lang['title_draft'] = "ドラフトを表示して編集";
-$lang['title_exit_draft'] = "現在の文書に戻る";
-$lang['draft_msg']= "この文書にはドラフトが存在します。「ドラフトを表示」「現在の文書に戻る」ボタンでドラフトと現在の文書を交互に表示できます。編集したい方を編集してください。" ;
-$lang['whats_this'] = "ヘルプ";
-$lang['complex_tables'] = "複雑な表を有効にする";
-$lang['minor_changes'] = "小さな変更";
-$lang['discard_edits'] = "編集内容を保存する場合はOKを、破棄する場合はキャンセルをクリックしてください。";
-
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['btn_dw_edit']           = 'テキストモードに変更';
+$lang['dw_btn_fck_preview']    = 'プレビュー';
+$lang['dw_btn_lang']           = '言語を選択';
+$lang['title_dw_delete']       = 'ページを削除';
+$lang['title_dw_edit']         = '内容を保存して編集を終わり、DokuWiki組み込みのエディタに切り替え';
+$lang['dw_btn_revert']         = '戻す';
+$lang['title_dw_revert']       = '以前のドラフトに戻す';
+$lang['title_dw_lang']         = 'スペルチェッカーの言語を選択';
+$lang['title_dw_cancel']       = '編集をキャンセル';
+$lang['btn_fck_edit']          = 'GUIモードに変更';
+$lang['confirm_delete']        = '本当にこのページを削除してもよろしいですか？';
+$lang['confirm_preview']       = '編集中の内容は保存されず失われます';
+$lang['dw_btn_backup']         = 'ドラフトを保存';
+$lang['title_dw_backup']       = '内容をドラフトに保存して、ロックの期限を更新';
+$lang['backup_empty']          = '以前のドラフトは存在しません。このまま戻すと編集中の内容はすべて失われますがよろしいですか？';
+$lang['btn_draft']             = 'ドラフトを表示';
+$lang['title_draft']           = 'ドラフトを表示して編集';
+$lang['btn_exit_draft']        = '現在の文書に戻る';
+$lang['title_exit_draft']      = '現在の文書に戻る';
+$lang['draft_msg']             = 'この文書にはドラフトが存在します。「ドラフトを表示」「現在の文書に戻る」ボタンでドラフトと現在の文書を交互に表示できます。編集したい方を編集してください。';
+$lang['whats_this']            = 'ヘルプ';
+$lang['complex_tables']        = '複雑な表を有効にする';
+$lang['minor_changes']         = '小さな変更';
+$lang['discard_edits']         = '編集内容を保存する場合はOKを、破棄する場合はキャンセルをクリックしてください。';
