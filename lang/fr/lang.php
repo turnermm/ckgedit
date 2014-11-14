95469c440bd8d500e647d2214b238180cec94cc5
TJ619 <rockandrollfever0@gmail.com>
2014-11-14 10:05:05 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author TJ619 <rockandrollfever0@gmail.com>
+ */
+$lang['dw_btn_lang']           = 'Langue';
+$lang['title_dw_delete']       = 'Supprimer la page';
+$lang['title_dw_edit']         = 'Sauvegarder le travail, quitter et retourner sur l\'éditeur native DokuWiki';
+$lang['title_dw_cancel']       = 'Quitter l\'éditeur';
+$lang['confirm_delete']        = 'Êtes-vous sûr de vouloir supprimer cette page ?';
+$lang['confirm_preview']       = 'Tout travail non sauvegardé sera perdu.';
+$lang['btn_exit_draft']        = 'Quitter le brouillon';
+$lang['minor_changes']         = 'Changements mineurs';
+$lang['dw_btn_styling']        = 'Éditer la police d\'écriture';
---
+++ lang/fr/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author TJ619 <rockandrollfever0@gmail.com>
+ */
+$lang['guest_toolbar']         = 'Afficher la barre d\'outil pour les invités';
+$lang['open_upload']           = 'Les invités peuvent uploader';
+$lang['create_folder']         = 'Activer le bouton de création de dossier dans le navigateur de fichier (o/n)';
+$lang['scayt_lang']            = 'Mettre le language SCAYT par défaut';
+$lang['dw_priority']           = 'Définir l\'éditeur DokuWiki comme éditeur par défaut';
+$lang['font_options']          = 'Retire les options de police d\'écriture';
+$lang['color_options']         = 'Retire les options de colorations';
