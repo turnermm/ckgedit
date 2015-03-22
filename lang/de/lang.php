530be032fa0e171bcd152d25ab5e7061de60f534
admin <admin@example.com>
2015-03-22 15:50:37 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,21 +1,25 @@
 <?php
 
-$lang['btn_dw_edit'] = "DW Edit";
-$lang['dw_btn_fck_preview']="CKG Vorschau";
-$lang['dw_btn_lang']="Sprache";
-$lang['title_dw_delete'] = "Seite l&ouml;schen";
-$lang['title_dw_edit'] = "Arbeit speichern, schliessen und zum DokuWiki Editor wechseln";
-$lang['dw_btn_revert'] = "R&uuml;ckg&auml;ngig";
-$lang['title_dw_revert'] = "Zur&uuml;ck zur Backup-Version";
-$lang['title_dw_lang']="Sprache f&uuml;r Rechtschreibekontrolle w&auml;hlen";
-$lang['title_dw_cancel']="Editor schliessen";
-$lang['btn_fck_edit'] = "CKG Editor";
-$lang['confirm_delete'] = "Sind Sie sicher, dass Sie diese Seite l&ouml;schen wollen?";
-$lang['confirm_preview'] = "Jede nicht gespeicherte Bearbeitung wird verloren gehen.";
-$lang['dw_btn_backup'] ="Backup";
-$lang['title_dw_backup'] ="Backup-Editor";
-$lang['backup_empty'] = "Es ist kein Backup vorhanden. Wollen Sie fortfahren?";
-$lang['btn_draft'] = "Entwurf &ouml;ffnen";
-$lang['title_draft'] = "Entwurf anzeigen und bearbeiten";
-$lang['btn_exit_draft'] = "Entwurf schliessen";
-$lang['draft_msg']= "F&uuml;r dieses Dokument besteht eine Entwurfsversion. Der Knopf 'Entwurf' schaltet zwischen diesem Dokument und dem Entwurf hin und her. Es k&ouml;nnen beide Dokumente bearbeitet oder gesichert werden." ;
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['btn_dw_edit']           = 'DW Edit';
+$lang['dw_btn_fck_preview']    = 'CKG Vorschau';
+$lang['dw_btn_lang']           = 'Sprache';
+$lang['title_dw_delete']       = 'Seite l&ouml;schen';
+$lang['title_dw_edit']         = 'Arbeit speichern, schliessen und zum DokuWiki Editor wechseln';
+$lang['dw_btn_revert']         = 'R&uuml;ckg&auml;ngig';
+$lang['title_dw_revert']       = 'Zur&uuml;ck zur Backup-Version';
+$lang['title_dw_lang']         = 'Sprache f&uuml;r Rechtschreibekontrolle w&auml;hlen';
+$lang['title_dw_cancel']       = 'Editor schliessen';
+$lang['btn_fck_edit']          = 'CKG Editor';
+$lang['confirm_delete']        = 'Sind Sie sicher, dass Sie diese Seite l&ouml;schen wollen?';
+$lang['confirm_preview']       = 'Jede nicht gespeicherte Bearbeitung wird verloren gehen.';
+$lang['dw_btn_backup']         = 'Backup';
+$lang['title_dw_backup']       = 'Backup-Editor';
+$lang['backup_empty']          = 'Es ist kein Backup vorhanden. Wollen Sie fortfahren?';
+$lang['btn_draft']             = 'Entwurf &ouml;ffnen';
+$lang['title_draft']           = 'Entwurf anzeigen und bearbeiten';
+$lang['btn_exit_draft']        = 'Entwurf schliessen';
+$lang['draft_msg']             = 'F&uuml;r dieses Dokument besteht eine Entwurfsversion. Der Knopf \'Entwurf\' schaltet zwischen diesem Dokument und dem Entwurf hin und her. Es k&ouml;nnen beide Dokumente bearbeitet oder gesichert werden.';
