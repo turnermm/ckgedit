30767a1f56224b2e5dc6e848b57e3fec1bbbf37f
Davor Turkalj <turki.bsc@gmail.com>
2015-05-14 16:10:15 +0200

    translation update

---
+++ lang/hr/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Davor Turkalj <turki.bsc@gmail.com>
+ */
+$lang['groups']                = 'Grupa koja može onemogućiti vrijeme zaključavanja (zastarjelo)';
+$lang['fck_preview']           = 'FCK grupa za pretpregled';
+$lang['guest_toolbar']         = 'Prikaži alatnu traku i gostima';
+$lang['guest_media']           = 'Gosti mogu se povezati na medijske datoteke';
+$lang['open_upload']           = 'Gosti mogu učitati';
+$lang['default_fb']            = 'Podrazumijevani pristup datotekama. Ako je ništa, ACL se ne primjenjuje.';
