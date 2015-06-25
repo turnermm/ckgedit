6794aa6e3a2d07e6b2103f72433e7df82781fbbb
serg <sergey_art82@inbox.ru>
2015-06-25 23:55:08 +0200

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -1,18 +1,25 @@
 <?php
 
 /**
- * Language file for ckgedit in UTF-8
- *  
- * @author:     Ianka Kryukov <IanisKr@mail.ru>
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Ianka Kryukov <IanisKr@mail.ru>
+ * @author serg <sergey_art82@inbox.ru>
  */
-
-$lang['btn_dw_edit'] = "Переключить в DokuWiki Редактор";
-$lang['dw_btn_fck_preview']="CKG предпросмотр";
-$lang['dw_btn_lang']="Язык";
-$lang['title_dw_delete'] = "Удалить страницу";
-$lang['title_dw_edit'] = "Сохранить текущий текст, выйти и переключить редактор с CKEditor на обычный DokuWiki Editor. (Поменяется при следующем редактировании.)";
-$lang['dw_btn_refresh'] ="Обновить";
-$lang['title_dw_lang']="Выбрать язык";
-$lang['title_dw_refresh']="Перезапустить таймер блокировки";
-$lang['title_dw_cancel']='Выйти из редактроа';
-$lang['btn_fck_edit'] = "Переключить в CKG Редактор";
+$lang['btn_dw_edit']           = 'Переключить в DokuWiki Редактор';
+$lang['dw_btn_fck_preview']    = 'CKG предпросмотр';
+$lang['dw_btn_lang']           = 'Язык';
+$lang['title_dw_delete']       = 'Удалить страницу';
+$lang['title_dw_edit']         = 'Сохранить текущий текст, выйти и переключить редактор с CKEditor на обычный DokuWiki Editor. (Поменяется при следующем редактировании.)';
+$lang['title_dw_lang']         = 'Выбрать язык';
+$lang['title_dw_cancel']       = 'Выйти из редактроа';
+$lang['btn_fck_edit']          = 'Переключить в CKG Редактор';
+$lang['confirm_delete']        = 'Уверены что хотите удалить страницу?';
+$lang['confirm_preview']       = 'Вся несохраненная работа будет потерянв';
+$lang['dw_btn_backup']         = 'Архивная копия';
+$lang['backup_empty']          = 'Буфер архивной копии выглядит пустым. Продолжить?';
+$lang['title_exit_draft']      = 'Возврат к текущему документу';
+$lang['draft_msg']             = 'За этим документом закреплен файл черновика. Кнопка черновик осуществляет переключение между этим документом и черновиком. Вы можете редактировать и сохранять каждую из них.';
+$lang['whats_this']            = 'Что это?';
+$lang['complex_tables']        = 'Включить Комплексные Таблицы';
+$lang['discard_edits']         = 'Для сохранения изменений нажмите ОК; для отмены нажмите Отмена';
