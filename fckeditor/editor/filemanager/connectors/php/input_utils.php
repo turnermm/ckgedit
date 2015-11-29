43c39c898ed5703f4cfcccd0c153eb15929cb83f
Myron Turner <turnermm02@shaw.ca>
2015-11-29 12:08:30 -0600

    few added vulnerability checks

--- fckeditor/editor/filemanager/connectors/php/input_utils.php
+++ fckeditor/editor/filemanager/connectors/php/input_utils.php
@@ -1,4 +1,10 @@
 <?php
+/**  
+ *  Connector to Dokuwiki Input class
+ *  sanitizes $_REQUEST variables
+ *  @author Myron Turner
+ */
+ 
 if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../../../../../../../').'/');
 require_once(DOKU_INC.'inc/Input.class.php');
 global $INPUT; 
