c133c8ffe61f437f368ddcbba2b22024153bd257
Myron Turner <turnermm02@shaw.ca>
2016-01-19 13:27:03 -0600

    fixed insertion of media links

--- fckeditor/editor/filemanager/browser/default/js/common.js
+++ fckeditor/editor/filemanager/browser/default/js/common.js
@@ -86,3 +86,13 @@ StringBuilder.prototype.ToString = function()
 {
     return this._Strings.join( '' ) ;
 }
+function ckgEedit_getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+} 
