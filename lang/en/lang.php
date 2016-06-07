8e1b4744ef726a24519db6b9138586b5fd2325b1
Myron Turner <turnermm02@shaw.ca>
2016-06-07 13:00:35 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,38 +1,41 @@
 <?php
 
-$lang['btn_dw_edit'] = "DW Edit";
-$lang['dw_btn_fck_preview']="CKG Preview";
-$lang['dw_btn_lang']="Language";
-$lang['title_dw_delete'] = "Delete Page";
-$lang['title_dw_edit'] = "Save Work, Exit, and Switch to Native DokuWiki Editor";
-$lang['dw_btn_revert'] = "Revert";
-$lang['title_dw_revert'] = "Revert to previous backup";
-$lang['title_dw_lang']="Select language for spell checker";
-$lang['title_dw_cancel']='Exit Editor';
-$lang['btn_fck_edit'] = "CKG Edit";
-$lang['confirm_delete'] = "Are you sure you want to delete this page?";
-$lang['confirm_preview'] = "Any work not saved will be lost.";
-$lang['editor_height'] = 'Editor Height';
-$lang['editor_height_title'] = 'Editor re-sizes on re-load';
-$lang['dw_btn_backup'] ="Back-up";
-$lang['title_dw_backup'] ="Back up editor window & Renew lock";
-$lang['backup_empty'] = "The back-up buffer appears to be empty. Do you wish to continue?";
-$lang['btn_draft'] = "Get Draft";
-$lang['title_draft'] = "View, Show, Edit Draft";
-$lang['btn_exit_draft'] = "Exit Draft";
-$lang['title_draft'] = "View, Show, Edit Draft";
-$lang['title_exit_draft'] = "Return to current document";
-$lang['draft_msg']= "This document has a draft file. The draft button toggles between the this document and the draft. You can edit and save either of the two." ;
-$lang['whats_this'] = "what's this?";
-$lang['complex_tables'] = "Enable Complex Tables";
-$lang['minor_changes'] = "Minor changes";
-$lang['discard_edits'] = "To Save your edits click OK; to Discard click Cancel";
-$lang['dw_btn_styling'] = "Edit Fonts";
-$lang['title_styling'] = 'Open with font styles shown as markup';
-$lang['js']['font_err_1'] = 'Font styling cannot be included in links. Click OK to accept the following correction:';
-$lang['js']['font_err_2'] = 'To revise, enter the full Dokuwiki link markup below, including brackets.  To return to Editor click Cancel.';
-$lang['js']['font_err_throw'] = 'Font Formatting Error';
-$lang['js']['dwp_save_err'] = 'Unable to save changes to:  ';
-$lang['js']['dwp_updated'] = 'Editor priority updated to: ';
-$lang['js']['dwp_not_sel'] = 'Editor priority not selected: it will be determined by dw_users group config setting';
-$lang['uprofile_title'] = "Select Your Default Editor";
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ */
+$lang['btn_dw_edit']           = 'DW Edit';
+$lang['dw_btn_fck_preview']    = 'CKG Preview';
+$lang['dw_btn_lang']           = 'Language';
+$lang['title_dw_delete']       = 'Delete Page';
+$lang['title_dw_edit']         = 'Save Work, Exit, and Switch to Native DokuWiki Editor';
+$lang['dw_btn_revert']         = 'Revert';
+$lang['title_dw_revert']       = 'Revert to previous backup';
+$lang['title_dw_lang']         = 'Select language for spell checker';
+$lang['title_dw_cancel']       = 'Exit Editor';
+$lang['btn_fck_edit']          = 'CKG Edit';
+$lang['confirm_delete']        = 'Are you sure you want to delete this page?';
+$lang['confirm_preview']       = 'Any work not saved will be lost.';
+$lang['editor_height']         = 'Editor Height';
+$lang['editor_height_title']   = 'Editor re-sizes on re-load';
+$lang['dw_btn_backup']         = 'Back-up';
+$lang['title_dw_backup']       = 'Back up editor window & Renew lock';
+$lang['backup_empty']          = 'The back-up buffer appears to be empty. Do you wish to continue?';
+$lang['btn_draft']             = 'Get Draft';
+$lang['title_draft']           = 'View, Show, Edit Draft';
+$lang['btn_exit_draft']        = 'Exit Draft';
+$lang['title_exit_draft']      = 'Return to current document';
+$lang['draft_msg']             = 'This document has a draft file. The draft button toggles between the this document and the draft. You can edit and save either of the two.';
+$lang['whats_this']            = 'what\'s this?';
+$lang['complex_tables']        = 'Enable Complex Tables';
+$lang['minor_changes']         = 'Minor changes';
+$lang['discard_edits']         = 'To Save your edits click OK; to Discard click Cancel';
+$lang['dw_btn_styling']        = 'Edit Fonts';
+$lang['title_styling']         = 'Open with font styles shown as markup';
+$lang['js']['font_err_1']      = 'Font styling cannot be included in links. Click OK to accept the following correction:';
+$lang['js']['font_err_2']      = 'To revise, enter the full Dokuwiki link markup below, including brackets.  To return to Editor click Cancel.';
+$lang['js']['font_err_throw']  = 'Font Formatting Error';
+$lang['js']['dwp_save_err']    = 'Unable to save changes to:  ';
+$lang['js']['dwp_updated']     = 'Editor priority updated to: ';
+$lang['js']['dwp_not_sel']     = 'Editor priority not selected: it will be determined by dw_users group config setting';
+$lang['uprofile_title']        = 'Select Your Default Editor';
