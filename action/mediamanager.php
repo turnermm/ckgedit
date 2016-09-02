0d57106af14baa0e9eca687a2a2c4f6a2fdf42b3
Steve Glick <sglick@superiorplastic.net>
2016-09-02 10:11:54 -0400

    use link type and alignment from file manager

--- action/mediamanager.php
+++ action/mediamanager.php
@@ -19,7 +19,10 @@ class action_plugin_ckgedit_mediamanager extends DokuWiki_Action_Plugin {
     function mediaman_started($event) {
         echo '<script type="text/javascript">window.onload = function () {
         dw_mediamanager.insert = function (id) {
+        //used by dokuwiki editor
         var opts, alignleft, alignright, edid, s;
+        //used by CKEditor
+        var link, align;
 
         // set syntax options
         dw_mediamanager.$popup.dialog(\'close\');
@@ -31,20 +34,24 @@ class action_plugin_ckgedit_mediamanager extends DokuWiki_Action_Plugin {
         if ({img: 1, swf: 1}[dw_mediamanager.ext] === 1) {
 
             if (dw_mediamanager.link === \'4\') {
-                    opts = \'?linkonly\';
+                    opts = "?linkonly";
+                    link = "linkonly";
             } else {
 
                 if (dw_mediamanager.link === "3" && dw_mediamanager.ext === \'img\') {
-                    opts = \'?nolink\';
+                    opts = "?nolink";
+                    link = "nolink";
                 } else if (dw_mediamanager.link === "2" && dw_mediamanager.ext === \'img\') {
-                    opts = \'?direct\';
+                    opts = "?direct";
+                    link = "direct";
+                } else {
+                    link = "detail";
                 }
 
                 s = parseInt(dw_mediamanager.size, 10);
-
                 if (s && s >= 1 && s < 4) {
                     opts += (opts.length)?\'&\':\'?\';
-                    opts += dw_mediamanager.size + \'00\';
+                    opts += dw_mediamanager.size + "00";
                     if (dw_mediamanager.ext === \'swf\') {
                         switch (s) {
                         case 1:
@@ -63,14 +70,40 @@ class action_plugin_ckgedit_mediamanager extends DokuWiki_Action_Plugin {
                     alignleft = dw_mediamanager.align === \'2\' ? \'\' : \' \';
                     alignright = dw_mediamanager.align === \'4\' ? \'\' : \' \';
                 }
+                switch (dw_mediamanager.align) {
+                case "1":
+                    align = "";
+                    break;
+                case "2":
+                    align = "medialeft";
+                    break;
+                case "3":
+                    align = "mediacenter";
+                    break;
+                case "4":
+                    align = "mediaright";
+                    break;
+                default:
+                    align = "baseline";
+                    break;
+                }
             }
         }
         edid = String.prototype.match.call(document.location, /&edid=([^&]+)/);
-        //opener.insertTags(edid ? edid[1] : \'wiki__text\',\'{{\'+alignleft+id+opts+alignright+\'|\',\'}}\',\'\');
 
         var funcNum = (location.search.split(\'CKEditorFuncNum=\')[1]||\'\').split(\'&\')[0];
             var fileUrl = DOKU_BASE + \'/lib/exe/fetch.php/\' + alignleft + id;
-            opener.CKEDITOR.tools.callFunction( funcNum, fileUrl);
+            opener.CKEDITOR.tools.callFunction( funcNum, fileUrl, function() {
+                var dialog = this.getDialog();
+                if ( dialog.getName() == "image" ) {
+                    if (align != null) {
+                        dialog.getContentElement("info", "cmbAlign").setValue(align);
+                    }
+                    if (link != null) {
+                        dialog.getContentElement("info", "cmbLinkType").setValue(link);
+                    }
+                }
+            });
         
         if(!dw_mediamanager.keepopen) {
             window.close();
