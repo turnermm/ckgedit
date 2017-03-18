250c04f3709cb5453028607379e88475fe281206
Myron Turner <turnermm02@shaw.ca>
2017-03-17 20:00:43 -0500

    support for smiley.local.conf

--- ckeditor/dwsmileys.php
+++ ckeditor/dwsmileys.php
@@ -5,4 +5,6 @@
 
 $SMILEYS = realpath(dirname(__FILE__).'/../../../../').'/conf/smileys.conf';
 if(!file_exists($SMILEYS)) $SMILEYS ='/etc/dokuwiki/smileys.conf';
+$LOCAL = preg_replace("/\.conf$/", '.local.conf', $SMILEYS) ;
+if(file_exists($LOCAL)) readfile($LOCAL);
 readfile($SMILEYS);
