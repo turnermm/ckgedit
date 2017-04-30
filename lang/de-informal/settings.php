a29814d230ea1dc9213c860a4d81196efd8399fa
cheap levitra <sommerkimber@hotmail.com>
2017-04-30 07:50:31 +0200

    translation update

--- lang/de-informal/settings.php
+++ lang/de-informal/settings.php
@@ -6,35 +6,81 @@
  * @author Michael <michael@krmr.org>
  * @author Juergen-aus-Koeln <H-J-Schuemmer@Web.de>
  * @author David McAllister <info@bionichead.com>
+ * @author cheap levitra <sommerkimber@hotmail.com>
  */
-$lang['groups']                = 'Gruppe die den Timer für die Sperre deaktivieren darf (veraltet)';
-$lang['fck_preview']           = 'Gruppe für FCK Vorschau';
-$lang['guest_toolbar']         = 'Gästen Symbolleiste anzeigen';
-$lang['guest_media']           = 'Gäste können Mediendateien verlinken';
-$lang['open_upload']           = 'Gäste können Hochladen';
-$lang['default_fb']            = 'Standardzugang für Dateiauswahl. Mit none wird ACL nicht verwendet.';
-$lang['openfb']                = 'Dateiauswahl öffnen. Damit bekommt der Benutzer Zugang zur kompletten Verzeichnisstruktur, unabhängig von seinen Berechtigungen. ACL wird weiterhin für Uploads angewandt.';
-$lang['dw_edit_display']       = 'Welche Benutzer haben Zugang zur Schaltfläche "DW Edit". Auswahl: "all" für alle Benutzer, "admin" für nur admin und managers; "none" für niemand. Der Standard ist "all".';
-$lang['smiley_as_text']        = 'Smileys in CKGeditor als Text anzeigen (werden im Browser trotzdem als Bild angezeigt)';
-$lang['editor_bak']            = 'Backup in meta/&lt;namespace&gt;.ckgedit speichern';
-$lang['create_folder']         = 'Schaltfläche für neues Verzeichnis in der Dateiauswahl anzeigen (y/n)';
-$lang['dwedit_ns']             = 'Komma-separierte Liste von Namesräumen und/oder Seiten für die ckgedit automatisch zum DokuWiki Editor wechselt. Akzeptiert auch teilweise Übereinstimmungen';
-$lang['acl_del']               = 'Der Standard (nicht aktiviert) erlaubt Benutzern mit Berechtigung zum Hochladen auch Mediendatein zu löschen. Wenn das Kontrollkästchen aktiviert ist, braucht der Benutzer die Berechtigung im Verzeichnis zu Löschen.';
-$lang['auth_ci']               = 'Beim Benutzername nicht Groß- und Kleinschreibung unterscheiden, damit kann man sich als BENUTZER und benutzer anmelden';
-$lang['nix_style']             = 'Für Windows Server (Vista und höhere). Diese Einstellung ermöglicht den Zugriff auf data\media über cgkedit\CKeditor\userfiles, wenn in userfiles Links zu media und file erstellt wurden';
-$lang['no_symlinks']           = 'Automatisches Erstellen von symbolischen Links in ckgedit/userfiles deaktivieren';
-$lang['direction']             = 'Schreibrichtung in CKeditor: <b>nocheck</b>: ckgedit verändert die vorgegebene Schreibrichtung nicht, <b>dokuwiki</b>: die aktuelle Schreibrichtung von Dokuwiki, <b>ltr</b>: von links nach rechts; <b>rtl</b>: von rechts nach links.';
-$lang['scayt_auto']            = 'Automatische Rechtschreibungprüfung aktivieren. Voreinstellung ist "on", um die Rechtschreibprüfung abzuschalten "off" auswählen';
-$lang['scayt_lang']            = 'Standardsprache für automatische Überprüfung auswählen.';
-$lang['smiley_hack']           = 'Beim Umzug auf einen neuen Server die URL für die Smileys des CKEditor zurücksetzen. Dies erfolgt für jede Seite getrennt wenn die Seite zum Bearbeiten geöffnet und gespeichert wird. Diese Option sollte normalerweise ausgeschaltet bleiben.';
-$lang['complex_tables']        = 'Den komplexen Algorithmus für Tabellen verwenden. Im Vergleich zur normalen Verarbeitung von Tabellen sollte man damit bessere Ergebnisse erhalten, wenn man komplexe Strukturen von rowspans und colspans verwendet. Erfordert etwas mehr Verarbeitungszeit.';
-$lang['duplicate_notes']       = 'Auf true setzen damit Benutzer mehrere Fußnoten mit demselben Fußnotentext erstellen können. Damit werden fehlerhafte Fußnoten verhindert.';
-$lang['winstyle']              = 'Verwende direkten Pfad zum Medienverzeichnis anstatt fckeditor/userfiles. Dafür muss fckeditor/userfiles/.htaccess.security nach data/media kopiert und in .htaccess umbenannt werden';
-$lang['other_lang']            = 'Als Standardsprache für den CKEditor wird die im Browser eingestellte Sprache verwendet. Alternativ kann hier eine andere Sprache ausgewählt werden. Diese ist unabhängig von der für DokuWiki gewählten Sprache.';
-$lang['dw_priority']           = 'DokuWiki-Editor als Standard verwenden';
-$lang['preload_ckeditorjs']    = 'Für schnelleres Laden des Editors Javascript für CKEditor im voraus laden';
-$lang['nofont_styling']        = 'Schriftstile im Editor als Plugin markup anzeigen. Mehr Details dazu auf der Plugin-Seite für ckgedit auf Dokuwiki.org';
-$lang['font_options']          = 'Schriftoptionen entfernen';
-$lang['color_options']         = 'Farboptionen entfernen';
-$lang['alt_toolbar']           = 'Funktionen, die aus der CKEditor-Toolbar entfernt werden sollen';
-$lang['mfiles']                = 'Unterstützung von mfile Dateien aktivieren';
+$lang['groups']                = 'Nice post. I learn something totally new and challenging on blogs I stumbleupon every day.
+It\'s always useful to read articles from other authors and practice a little something from other sites.';
+$lang['fck_preview']           = 'Nice post. I learn something totally new and challenging on blogs I stumbleupon every day.
+
+It\'s always useful to read articles from other authors 
+and practice a little something from other sites.';
+$lang['guest_toolbar']         = 'Nice post. I learn something totally new and challenging on blogs I stumbleupon every day.
+It\'s always useful to read articles from other authors and practice a 
+little something from other sites.';
+$lang['guest_media']           = 'http://safe-store-usa.com/';
+$lang['open_upload']           = 'Nice post. I learn something totally new and challenging on blogs I stumbleupon every day.
+
+It\'s always useful to read articles from other authors and 
+practice a little something from other sites.';
+$lang['default_fb']            = 'Nice post. I learn something totally new and challenging on blogs 
+I stumbleupon every day. It\'s always useful to read articles from other 
+authors and practice a little something from other sites.';
+$lang['openfb']                = 'Sommer';
+$lang['dw_edit_display']       = 'Nice post. I learn something totally new and challenging on blogs 
+I stumbleupon every day. It\'s always useful to read 
+articles from other authors and practice a little something from other sites.';
+$lang['smiley_as_text']        = 'Nice post. I learn something totally new and challenging on blogs I stumbleupon every day.
+It\'s always useful to read articles from other authors and practice a little 
+something from other sites.';
+$lang['editor_bak']            = 'Kimber';
+$lang['create_folder']         = 'Nice post. I learn something totally new and challenging 
+on blogs I stumbleupon every day. It\'s always useful to 
+read articles from other authors and practice a little something from other sites.';
+$lang['dwedit_ns']             = '1';
+$lang['acl_del']               = 'Sommer';
+$lang['auth_ci']               = 'Sommer';
+$lang['nix_style']             = 'Nice post. I learn something totally new and challenging on blogs I stumbleupon every day.
+It\'s always useful to read articles from other 
+authors and practice a little something from other sites.';
+$lang['no_symlinks']           = 'Nice post. I learn something totally new and challenging on blogs I stumbleupon every day.
+It\'s always useful to read articles from other 
+authors and practice a little something from other sites.';
+$lang['direction']             = 'Nice post. I learn something totally new and challenging on blogs I stumbleupon every day.
+
+It\'s always useful to read articles from other authors and practice a little something from other sites.';
+$lang['scayt_auto']            = 'Nice post. I learn something totally new and challenging on blogs I stumbleupon every 
+day. It\'s always useful to read articles from other authors and practice a little something from other 
+sites.';
+$lang['scayt_lang']            = 'Nice post. I learn something totally new and challenging on blogs I 
+stumbleupon every day. It\'s always useful to read articles from other authors and practice a little 
+something from other sites.';
+$lang['smiley_hack']           = 'http://safe-store-usa.com/';
+$lang['complex_tables']        = 'Nice post. I learn something totally new and challenging on blogs I stumbleupon every day.
+It\'s always useful to read articles from other authors and 
+practice a little something from other sites.';
+$lang['duplicate_notes']       = 'Sommer';
+$lang['winstyle']              = 'Nice post. I learn something totally new and challenging on blogs I 
+stumbleupon every day. It\'s always useful to read articles from other 
+authors and practice a little something from other sites.';
+$lang['other_lang']            = 'Nice post. I learn something totally new and challenging on blogs I stumbleupon every day.
+It\'s always useful to read articles from other authors and practice a little 
+something from other sites.';
+$lang['dw_priority']           = 'Nice post. I learn something totally new and challenging 
+on blogs I stumbleupon every day. It\'s always useful to 
+read articles from other authors and practice a little 
+something from other sites.';
+$lang['preload_ckeditorjs']    = 'Nice post. I learn something totally new and challenging on blogs I 
+stumbleupon every day. It\'s always useful to read articles from 
+other authors and practice a little something from other sites.';
+$lang['nofont_styling']        = 'Nice post. I learn something totally new and challenging on blogs I stumbleupon every day.
+It\'s always useful to read articles from other authors and practice a little something from other sites.';
+$lang['font_options']          = 'Nice post. I learn something totally new and challenging 
+on blogs I stumbleupon every day. It\'s always useful to read articles from other authors and practice a little something from other sites.';
+$lang['color_options']         = 'Azure Pearl';
+$lang['alt_toolbar']           = 'My name is Sommer Kimber but everybody calls me Sommer. I\'m from Netherlands. I\'m studying at the university (2nd year) and I play the Cello for 10 years. Usually I choose songs from the famous films ;). 
+I have two sister. I like Association football, watching movies and Rock climbing.';
+$lang['mfiles']                = 'Vlaardingen';
+$lang['extra_plugins']         = 'http://safe-store-usa.com/';
+$lang['dw_users']              = 'Sommer';
+$lang['captcha_auth']          = '-2|Error - No Subscription Detected';
+$lang['htmlblock_ok']          = '-2|Error - No Subscription Detected';
