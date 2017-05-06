149d0ba5a3277e0c0c8db3d38d806f53b6a51b4e
Myron Turner <turnermm02@shaw.ca>
2017-05-06 09:04:03 -0500

    updated Geshi German lang file

--- ckeditor/plugins/geshi/lang/de.js
+++ ckeditor/plugins/geshi/lang/de.js
@@ -1,3 +1,11 @@
 CKEDITOR.plugins.setLang("geshi","de",{
-    	title: 'Geshi Snippet oder Block'
+    	title: 'Geshi Snippet oder Block',
+        lang: 'Sprache',
+        which: 'Code-Schnippsel?',
+        codeblock: 'Code-Schnippsel',
+        snippet: 'Herunterladbarer Code-Schnippsel',
+        lang_empty: 'Das Sprach-Feld darf nicht leer sein.',
+        code: 'Text für Code-Schnippsel',
+        code_empty: 'Code-Schnippsels erforderlich.',
+        file: 'Dateiname des Code-Schnippsels',
 });
