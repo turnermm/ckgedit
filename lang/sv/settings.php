13f1b292a94a85331b3038bc447a5f470bfba4a2
Tor Härnqvist <tor@harnqvist.se>
2017-12-20 19:20:15 +0100

    translation update

---
+++ lang/sv/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['guest_toolbar']         = 'Visa verktygsfält för besökare';
+$lang['guest_media']           = 'Besökare kan länka till mediafiler';
+$lang['open_upload']           = 'Besökare kan ladda uopp';
+$lang['create_folder']         = 'Aktivera knapp för att skapa ny katalog i filhanteraren (j/n)';
+$lang['scayt_lang']            = 'Välj SCAYT-standardspråk';
+$lang['font_options']          = 'Ta bort typsnittsalternativ.';
+$lang['color_options']         = 'Ta bort färgalternativ.';
+$lang['mfiles']                = 'Aktivera mfile-stöd';
+$lang['rel_links']             = 'Aktivera stöd för relativa interna- och bildlänkar';
