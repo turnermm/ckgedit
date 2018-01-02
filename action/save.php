b6d3348aa7577498b1d25841456138f0818ec75e
Myron Turner <turnermm02@shaw.ca>
2018-01-02 08:35:23 -0600

    update fix to deleted backslash in code blocks

--- action/save.php
+++ action/save.php
@@ -264,7 +264,7 @@ Removed newlines and spaces from beginnings and ends of text enclosed by font ta
 
       /* remove extra line-feeds following in-table code blocks */ 
       $TEXT = preg_replace_callback(
-       '#(code|file)\>\s*.*?\n?\|#ms',
+       '#(/code|/file)\>\s*.*?\n?\|#ms',
        function($matches) {         
          $matches[0] = preg_replace("/([\S\s\w\:])\\\\(\w)/ms","$1@#@$2",$matches[0]); //retain backslashes inside code blocks
          $matches[0] = preg_replace("/(\w+)\\\\/ms","$1@#@",$matches[0]);
