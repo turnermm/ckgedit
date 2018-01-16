d38244050c0c9e572a61ea85c8d61445f96140ce
Myron Turner <turnermm02@shaw.ca>
2018-01-16 10:45:39 -0600

    fix corrupted image as link when checking for userheading and relative links

--- action/save.php
+++ action/save.php
@@ -181,8 +181,18 @@ class action_plugin_ckgedit_save extends DokuWiki_Action_Plugin {
            '#\[\[(.*?)\]\]#ms',
                function($matches){ 
                     global $ID, $conf;      
+                    if(preg_match("/\[\[http/",$matches[0])) return $matches[0];  //not an internal link
+                   
+                    if (preg_match("/\[\[.*?|\{\{\]\]/", $matches[0])) {   //internal link with image link
+                        if(!$this->getConf('rel_links')) { 
+                            return $matches[0];
+                        }
+                      $link = explode('?',$matches[1]); 
+                      list($link_id,$linktext) = explode('|', $link[0]); 
+                      $current_id = $this->abs2rel($link_id,$ID); 
+                      return preg_replace("#$link_id#",$current_id, $matches[0]);                      
+                   }                
                 
-                   if(preg_match("/\[\[http/",$matches[0])) return $matches[0];  //not an internal link
                    $link = explode('?',$matches[1]);
                    list($link_id,$linktext) = explode('|', $link[0]);          
                    if($this->getConf('rel_links')) 
