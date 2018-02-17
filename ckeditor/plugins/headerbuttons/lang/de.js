fb07d1443a2f92fb871fdb6a3550e1a5b18f1981
Myron Turner <turnermm02@shaw.ca>
2018-02-17 15:16:20 -0600

    localization support for headerbuttons plugin

---
+++ ckeditor/plugins/headerbuttons/lang/de.js
@@ -0,0 +1,8 @@
+CKEDITOR.plugins.setLang( 'headerbuttons', 'de', {
+     same:     'gleiche Überschrift',   
+	lower:     'untergeordnete Überschrift',
+	higher:    'höhere Überschrift',
+	remove: 'Überschrift entfernen',
+} );
+
+//editor.lang.headerbuttons.same
---
+++ ckeditor/plugins/headerbuttons/lang/en.js
@@ -0,0 +1,9 @@
+CKEDITOR.plugins.setLang( 'headerbuttons', 'en', {
+    same:   'Same Level Header',   
+	lower:  'Lower Level Header',
+	higher: 'Higher Level Header',
+	remove: 'Remove Header'
+
+} );
+
+//editor.lang.headerbuttons.same
