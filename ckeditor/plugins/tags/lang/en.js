9b36e8902151c15b43e413d211871959d50b5fb8
Myron Turner <turnermm02@shaw.ca>
2018-07-31 17:09:57 -0500

    added updated  en.js for tags plugin

--- ckeditor/plugins/tags/lang/en.js
+++ ckeditor/plugins/tags/lang/en.js
@@ -1,4 +1,7 @@
 CKEDITOR.plugins.setLang( 'tags', 'en', {        
         dlgTitle: 'Tags',
-        customTags:  'Custom Tags (Space separated list of key words; spaces in key words should be replaced by underscores. ) :'
+        customTags:  'Custom Tags (Space separated list of key words; spaces in key words should be replaced by underscores. ) :',
+        info:  'If <code>Cursor</code> is selected, the old tag syntax and its contents must also be selected; it will replaced by the new tag markup. If there are no old tags, the the new tag(s) will  be inserted at the cursor.',
+        bottom: 'Bottom',
+        cursor: 'Cursor'
 } );
