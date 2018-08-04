0f8af03ab4f10374ccad691892917fd3e9897875
Myron Turner <turnermm02@shaw.ca>
2018-08-04 12:46:34 -0500

    Moved jQuery ajax call from tags.js to plugin.js an made call async.

--- ckeditor/plugins/tags/plugin.js
+++ ckeditor/plugins/tags/plugin.js
@@ -14,7 +14,31 @@ CKEDITOR.plugins.add("tags",
         }
       );
       var tags_position = 'top';
-      editor.tags_pos = function() { return tags_position}
+      editor.tags_pos = function() { return tags_position};
+	  var existingTags;
+	  editor.existing_tags = function() { return existingTags};
+	  jQuery.ajax(
+          DOKU_BASE + 'lib/exe/ajax.php',
+          {
+            data:
+              {
+                call: 'tagapi_list'
+              },
+            type: "POST",
+          //  async: false,
+            dataType: "json",
+            success: function(data, textStatus, jqXHR)
+              {
+                existingTags = data.tags;
+              // alert(existingTags);
+              },
+            error: function(jqXHR, textStatus, errorThrown )
+              {
+                alert(textStatus);
+                alert(errorThrown);
+              }
+          }
+        );
     }
   }
 );
