5f05ed5c37ea4b06afa45d593209aaf3abf00d72
Myron Turner <turnermm02@shaw.ca>
2018-08-05 16:07:58 -0500

    Upgraded ckeditor -plugins -> tag

--- ckeditor/plugins/tags/lang/en.js
+++ ckeditor/plugins/tags/lang/en.js
@@ -1,4 +1,8 @@
 CKEDITOR.plugins.setLang( 'tags', 'en', {        
         dlgTitle: 'Tags',
-        customTags:  'Custom Tags (Space separated list of key words; spaces in key words should be replaced by underscores. ) :'
+        customTags:  'Custom Tags (Space separated list of key words; spaces in key words should be replaced by underscores. ) :',
+        info:  'If <code>Cursor</code> is chosen, the previous tag syntax and its contents must  be selected (highlighted) with the mouse. <br /> It will replaced by the new tag markup. If there are no old tags, the the new tag(s) will  be inserted at the cursor.',
+        bottom: 'Page Bottom',
+        cursor: 'Cursor',
+        top: 'Page Top'
 } );
--- ckeditor/plugins/tags/plugin.js
+++ ckeditor/plugins/tags/plugin.js
@@ -13,6 +13,32 @@ CKEDITOR.plugins.add("tags",
           icon: this.path + 'images/icon.png'
         }
       );
+      var tags_position = 'top';
+      editor.tags_pos = function() { return tags_position};
+	  var existingTags;
+	  editor.existing_tags = function() { return existingTags};
+	  jQuery.ajax(
+          DOKU_BASE + 'lib/exe/ajax.php',
+          {
+            data:
+              {
+                call: 'tagapi_list'
+              },
+            type: "POST",
+          //  async: false,
+            dataType: "json",
+            success: function(data, textStatus, jqXHR)
+              {
+                existingTags = data.tags;
+              // alert(existingTags);
+              },
+            error: function(jqXHR, textStatus, errorThrown )
+              {
+                alert(textStatus);
+                alert(errorThrown);
+              }
+          }
+        );
     }
   }
 );
