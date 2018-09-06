f571412f6df6e6d1fc44abfc2f4f686cd18f12d2
Myron Turner <turnermm02@shaw.ca>
2018-09-06 17:22:19 -0500

    Current using geshi plugin scripts to work up msmenu plugin

---
+++ ckeditor/plugins/msword/lang/en.js
@@ -0,0 +1,13 @@
+CKEDITOR.plugins.setLang("geshi","en",{
+    	title: 'Geshi snippet or block',
+        lang: 'Select language',
+        which: 'Codeblock or Snippet?',
+        codeblock: 'Codeblock',
+        snippet: 'Downloadable Snippet',
+        lang_empty: 'Language field cannot be empty.',
+        code: 'Text for Snippet or Codeblock',
+        code_empty: 'Snippet or Codeblock required.',
+        file: 'Snippet File name',
+        quick_srch: 'Quick search',
+        tooltip: 'Enter full or partial name,  click to search language menu',
+});
---
+++ ckeditor/plugins/msword/lang/fr.js
@@ -0,0 +1,3 @@
+CKEDITOR.plugins.setLang("geshi","fr",{
+    	title: 'Extrait ou bloc Geshi'
+});
