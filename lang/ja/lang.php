0129c035acc8f6fe6963d7847ef8b9f772c1caad
HokkaidoPerson <dosankomali@yahoo.co.jp>
2018-09-07 15:15:37 +0200

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author HokkaidoPerson <dosankomali@yahoo.co.jp>
  * @author Hideaki SAWADA <chuno@live.jp>
  */
 $lang['btn_dw_edit']           = 'テキストモードに変更';
@@ -33,3 +34,31 @@ $lang['minor_changes']         = '小さな変更';
 $lang['discard_edits']         = '編集内容を保存する場合はOKを、破棄する場合はキャンセルをクリックしてください。';
 $lang['dw_btn_styling']        = 'フォントの編集';
 $lang['title_styling']         = 'マークアップで指定されたフォントスタイルで表示する。';
+$lang['js']['font_err_1']      = 'フォントの装飾はリンクに含める事が出来ません。以下の修正を承諾するには「OK」をクリックして下さい。';
+$lang['js']['font_err_2']      = '修正するには、以下にDokuWikiのリンクの完全なマークアップを入力して下さい。ブラケットも含んで下さい。エディターに戻るには「キャンセル」をクリックして下さい。';
+$lang['js']['font_err_throw']  = 'フォント書式エラー';
+$lang['js']['dwp_save_err']    = '以下に変更を保存出来ません：';
+$lang['js']['dwp_updated']     = 'エディターの優先度は次の通り更新されました：';
+$lang['js']['dwp_not_sel']     = 'エディターの優先度が選択されていません。優先度はdw_usersグループの構成設定で指定します。';
+$lang['js']['mediamgr_notice'] = '挿入にリンクダイアログを使用する';
+$lang['js']['font_conflict']   = '分析した所、リンクフォントの競合が1つ以上見つかりました。「OK」をクリックすると、エディターに戻ってフォントスタイルを除去する事が出来ます。より詳しい情報については、https://www.dokuwiki.org/plugin:ckgedit:font_styling#conflicts_with_dokuwiki_links を参照して下さい。';
+$lang['js']['ckg_paste_restart'] = 'エディターを再読み込みすると、補助的な貼り付けサポートが有効になります';
+$lang['js']['on']              = 'ON';
+$lang['js']['off']             = 'OFF';
+$lang['mediamgr_imgonly']      = '画像ダイアログは、画像に対してのみ用いて下さい。他のメディアについてはリンクダイアログを用いて下さい。選択したファイル：';
+$lang['uprofile_title']        = '使いたいデフォルトのエディターを選択';
+$lang['btn_val_dw_fb']         = 'DWファイルブラウザー';
+$lang['btn_val_ckg_fb']        = 'CKGファイルブラウザー';
+$lang['btn_title_dw_fb']       = '保存してエディターを閉じ、DWファイルブラウザーに切り替える';
+$lang['btn_title_ckg_fb']      = '保存してエディターを閉じ、CKGファイルブラウザーに切り替える';
+$lang['formatdel']             = 'ヘッダーからマークアップを除去しました。DokuWikiでサポートされていません。';
+$lang['fontdel']               = 'リンクからフォントマークアップを除去しました。DokuWikiでサポートされていません。';
+$lang['ckgcke_conflict']       = 'ckgeditのコピーが検出されました。一方もしくは両方を無効化する必要があります。';
+$lang['ws_cantcopy']           = 'winstyleセットアップに際して：次に対してコピー出来ません：';
+$lang['ws_copiedhtaccess']     = 'winstyleセットアップに際して、セキュリティ設定された.htaccessファイルを、ディレクトリ data・mediaにコピーしました
+ckgedit/fckeditor/userfiles/.htacess.security をご覧下さい';
+$lang['userfiles_perm']        = 'パーミッションを確認して下さい。ckgeditがアクセス出来ません。';
+$lang['sym_not created_1']     = '作成出来ません';
+$lang['sym_not created_2']     = 'シンボリックリンクをファイルブラウザーに作成出来ません。以下にアクセス出来ません：';
+$lang['sym_not created_3']     = '次に対しシンボリックリンクを作成しようとした際にエラーが発生しました：';
+$lang['syms_created']          = '以下のリンクがユーザーファイルのディレクトリに作成されました。';
