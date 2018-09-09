0e22a0f1403418b114961421829e6cd6ea1640a2
Myron Turner <turnermm02@shaw.ca>
2018-09-09 15:41:50 -0500

    merge errors correction

--- ckeditor/plugins/msword/lang/en.js
+++ ckeditor/plugins/msword/lang/en.js
@@ -1,8 +1,4 @@
 CKEDITOR.plugins.setLang("msword","en",{
     	title: 'Paste from Word',
-<<<<<<< HEAD
         info: 'Insert your text into the box above using Ctrl+V, and then click <span style="font-weight:bold;font-size:11pt;">OK</span>' ,
-=======
-        info: 'Insert your text into the box above and click <span style="font-weight:bold;font-size:11pt;">OK</span>' ,
->>>>>>> 91a372e1fbdf0b185b6adaa2277c69edc2982db9
 });
