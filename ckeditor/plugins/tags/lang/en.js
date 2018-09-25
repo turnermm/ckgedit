cfe5c9322fb7a7ec0b19b515762412693351d11a
Myron Turner <turnermm02@shaw.ca>
2018-09-25 10:58:54 -0500

    Added localization label to tags.js

--- ckeditor/plugins/tags/lang/en.js
+++ ckeditor/plugins/tags/lang/en.js
@@ -4,5 +4,6 @@ CKEDITOR.plugins.setLang( 'tags', 'en', {
         info:  'If <code>Cursor</code> is chosen, the previous tag syntax and its contents must  be selected (highlighted) with the mouse. <br /> It will replaced by the new tag markup. If there are no old tags, the the new tag(s) will  be inserted at the cursor.',
         bottom: 'Page Bottom',
         cursor: 'Cursor',
-        top: 'Page Top'
+        top: 'Page Top',
+        radioLabel: 'Place Tags at:',
 } );
