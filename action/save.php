64059f9df11f1c1195cba2f84e3423c1c007e082
Myron Turner <turnermm02@shaw.ca>
2019-09-13 09:21:07 -0500

    Finalizes restructuring of syntax highlighting with numbers

--- action/save.php
+++ action/save.php
@@ -177,11 +177,13 @@ class action_plugin_ckgedit_save extends DokuWiki_Action_Plugin {
             $TEXT = str_replace('L_PARgr', '(',$TEXT);
             $TEXT = str_replace('R_PARgr', ')',$TEXT);
        } 
-       
-    $TEXT = preg_replace_callback("#<code\s+(\w+)>.*?(\[enable_line_numbers\s*=&quot;\s*\w{3,5}&quot;\]?.*?\])\s*\*\/#ms",
-      function($matches) {
-           return '<code ' . $matches[1] . $matches[2] .'>';
-      }, $TEXT
+/*
+Restructure numbered syntax highlighting  13/09/2019
+*/
+$TEXT = preg_replace_callback("#<code\s+(\w+)>.*?(\[enable_line_numbers.*?\])\s*\*\/#ms",
+    function($matches) {
+        return '<code ' . $matches[1] .' ' . $matches[2] .'>';
+    }, $TEXT
     ) ;
        
         $this->replace_entities();
