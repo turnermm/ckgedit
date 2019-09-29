76f1ef26d49be614f14a1ac2c2469957894c2bda
Gerrit Uitslag <klapinklapin@gmail.com>
2019-09-29 15:25:27 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -3,11 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Gerrit Uitslag <klapinklapin@gmail.com>
  * @author Coen Eisma <info@coeneisma.nl>
  * @author Mijndert <mijndert@mijndertstuij.nl>
  * @author Peter van Diest <peter.van.diest@xs4all.nl>
  */
-$lang['btn_dw_edit']           = 'DW Editor';
+$lang['btn_dw_edit']           = 'DW Bewerkvenster';
 $lang['dw_btn_fck_preview']    = 'CKG Voorbeeld';
 $lang['dw_btn_lang']           = 'Taal';
 $lang['title_dw_delete']       = 'Pagina schrappen';
@@ -19,10 +20,10 @@ $lang['title_dw_cancel']       = 'Stop Editor';
 $lang['btn_fck_edit']          = 'CKG Editor';
 $lang['confirm_delete']        = 'Wil je deze pagina schrappen? Zeker?';
 $lang['confirm_preview']       = 'Niet opgeslagen werk gaat verloren.';
-$lang['editor_height']         = 'Editorhoogte';
+$lang['editor_height']         = 'Hoogte bewerkvenster';
 $lang['editor_height_title']   = 'De afmetingen van de editor worden opnieuw bepaald bij herladen';
 $lang['dw_btn_backup']         = 'Back-up';
-$lang['title_dw_backup']       = 'Back up editor venster & hernieuw vergrendeling';
+$lang['title_dw_backup']       = 'Maak een back-up van bewerkvenster & hernieuw vergrendeling';
 $lang['backup_empty']          = 'De back-up buffer is blijkbaar leeg. Verder gaan?';
 $lang['btn_draft']             = 'Ontwerp';
 $lang['title_draft']           = 'Bekijk, Toon, Bewerk ontwerp';
@@ -36,14 +37,18 @@ $lang['discard_edits']         = 'Om je wijzigingen op te slaan klik OK; voor af
 $lang['dw_btn_styling']        = 'Lettertypen aanpassen';
 $lang['title_styling']         = 'Open met de stijl van het lettertype getoond als markup';
 $lang['js']['font_err_1']      = 'Font styling niet toegestaan in links. Klik OK om de volgende correctie te accepteren.';
+$lang['js']['font_err_2']      = 'Om het bij te werken, voer de volledige syntaxis van de DokuWiki-link in, inclusief de haakjes. Om terug te keren naar het bewerkvenster klikt u op Annuleren.';
 $lang['js']['font_err_throw']  = 'Font-opmaakfout';
 $lang['js']['dwp_save_err']    = 'Niet mogelijk om veranderingen op te slaan in:';
 $lang['js']['dwp_updated']     = 'Prioriteitseditor geüpdatete naar:';
 $lang['js']['dwp_not_sel']     = 'Prioriteitseditor niet geselecteerd. Het zal bepaald worden op basis van de dw_user group config setting';
 $lang['js']['mediamgr_notice'] = 'Gebruik de linkdialoogvenster om in te voegen';
 $lang['js']['font_conflict']   = 'De parser heeft één of meer  link-font-conflicten gevonden. Door op OK te klikken ga je terug naar de editor waar je de fontstijlen kunt verwijderen. Voor meer informatie kijk je op: https://www.dokuwiki.org/plugin:ckgedit:font_styling#conflicts_with_dokuwiki_links';
+$lang['js']['ckg_paste_restart'] = 'Bij het herladen van het bewerkvenster, wordt de ondersteuning voor plakken aangezet';
 $lang['js']['on']              = 'Aan';
 $lang['js']['off']             = 'Uit';
+$lang['js']['broken_image_1']  = 'Gebruik de imagePaste plugin met de DokuWiki Mediabeheerder of probeer de MS Word plak-functie';
+$lang['js']['broken_image_2']  = 'Ctrl+V van afbeeldingen met ruwe bestandsgrootte groter dan 2.5 MB kunnen blijven hangen. Deze afbeelding is:';
 $lang['mediamgr_imgonly']      = 'Upload alleen afbeeldingen wanneer je het afbeeldingsdialoogvenster gebruikt.';
 $lang['uprofile_title']        = 'Selecteer je standaard editor';
 $lang['btn_val_dw_fb']         = 'DW bestandsbrowser';
@@ -52,6 +57,7 @@ $lang['btn_title_dw_fb']       = 'Sla op en sluit de editor en schakel over naar
 $lang['btn_title_ckg_fb']      = 'Sla op en sluit de editor en schakel over naar de CKG bestandsbrowser';
 $lang['formatdel']             = 'Opmaak verwijderd van de kopteksten: niet ondersteund door Dokuwiki';
 $lang['fontdel']               = 'Letteropmaak verwijderd uit links: niet ondersteund door Dokuwiki';
+$lang['ckgcke_conflict']       = 'Er is een kopie van ckgedit gedetecteerd. Ckgedit of ckgdoku moet zijn uitgeschakeld.';
 $lang['ws_cantcopy']           = 'Voor winstyle-instellingen: kan niet kopiëren naar';
 $lang['ws_copiedhtaccess']     = 'Voor winstyle-instellingen: de beveiligingsinstellingen van .htaccess zijn naar data/media gekopieerd
 Kijk op ckgedit/fckeditor/userfiles/.htacess.security';
@@ -63,6 +69,7 @@ $lang['syms_created']          = 'De volgende links zijn gecreëerd in de gebrui
 $lang['dblclk']                = '<b>Nieuwe optie:</b> dubbelklik in de browser om de DW Editor te openen om de sectie waar de cursor staat te bewerken. <b>Kijk op: </b><a href=\'https://www.dokuwiki.org/plugin:ckgedit#direct_access_to_dokuwiki_editor\'>direct_access_to_dokuwiki_editor</a>. ';
 $lang['dblclk_reminder']       = '<b>Herinnering</b>';
 $lang['ckg_img_paste']         = 'Ondersteuning voor aanvullende afbeelding plakken aanzetten';
-$lang['ckg_img_paste_title']   = 'Uit om dubbel plakken te voorkomen of om plakken in te schakelen';
+$lang['ckg_img_paste_title']   = 'Uit om dubbel plakken te voorkomen of Aan om plakken in te schakelen';
 $lang['on']                    = 'aan';
 $lang['off']                   = 'uit';
+$lang['broken_image']          = 'Gebruik de imagePaste plugin met de DokuWiki Mediabeheerder of probeer de MS Word plak-functie. Ctrl+V van afbeeldingen met ruwe bestandsgrootte groter dan 2.5 MB kunnen blijven hangen. Deze afbeelding is:';
--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Gerrit Uitslag <klapinklapin@gmail.com>
  * @author Coen Eisma <info@coeneisma.nl>
  * @author Mijndert <mijndert@mijndertstuij.nl>
  * @author Peter van Diest <peter.van.diest@xs4all.nl>
@@ -12,24 +13,24 @@ $lang['fck_preview']           = 'FCK Voorbeeld Groep';
 $lang['guest_toolbar']         = 'Toon werkbalk aan Gast';
 $lang['guest_media']           = 'Gast mag Media bestanden linken';
 $lang['open_upload']           = 'Gast mag uploaden';
-$lang['default_fb']            = 'Verstek bestandsbenadering. Zonder (none) verstekwaarde is ACL niet van toepassing.';
+$lang['default_fb']            = 'Standaard bestandstoegang permissies. Als \'none\', dan wordt ACL niet toegepast.';
 $lang['openfb']                = 'Open bestandsbenadering. De gebruiker krijgt hierdoor toegang tot de volledige directory structuur, ongeacht de gebruikersrechten dit al dan niet toelaten. ACL is wel van toepassing op uploads.';
-$lang['dw_edit_display']       = 'Welke gebruikers mogen de "DW Editor" knop gebruiken. Selecteer: "all" voor alle gebruikers; "admin" voor administrator en beheerders; "none" voor niemand. Verstekwaarde is "all".';
+$lang['dw_edit_display']       = 'Welke gebruikers mogen de "DW Editor" knop gebruiken. Selecteer: "all" voor alle gebruikers; "admin" voor administrator en beheerders; "none" voor niemand. Standaardwaarde is "all".';
 $lang['smiley_as_text']        = 'Toon smileys als tekst in FCKeditor (wordt wel als afbeelding getoond in de browser)';
 $lang['editor_bak']            = 'Sla backup op in meta/&lt;namespace&gt;.ckgedit';
 $lang['create_folder']         = 'Activeer knop Aanmaken map in bestandsverkenner (y/n)';
-$lang['dwedit_ns']             = 'Comma gescheiden lijst van namespaces waarbij ckgedit automatisch terugschakeltnaar de oorspronkelijke DokuWiki Editor.';
-$lang['acl_del']               = 'Verstekwaarde (niet aangevinkt) voor schrappen van media bestanden voor gebruikers met upload rechten; indien aangevinkt, dan heeft gebruiker toelating nodig om bestand uit de map te schrappen.';
+$lang['dwedit_ns']             = 'Komma-gescheiden lijst van namespaces waarbij ckgedit automatisch terugschakelt naar de oorspronkelijke DokuWiki Editor.';
+$lang['acl_del']               = 'Standaardinstelling (niet aangevinkt) staat gebruikers met upload rechten toe om mediabestanden te verwijderen; indien aangevinkt, dan heeft gebruiker de verwijder-permissie nodig om het bestand uit de map te verwijderen.';
 $lang['auth_ci']               = 'De gebruikers login is niet hoofdletter gevoelig, dus je kan inloggen met zowel GEBRUIKER als gebruiker';
-$lang['nix_style']             = 'Voor Windows Servers (Vista en later).  Deze parameter maakt het mogelijk om data\media te benaderen met ckgeditckeditor\userfiles, indien links naar media en bestand succesvol aangemaakt zijn in userfiles';
-$lang['no_symlinks']           = 'Desactiveer automatische creatie van symbolische links in ckgedit/userfiles.';
-$lang['direction']             = 'Zet de schrijf en lees richting van de taal in FCKeditor:  <b>nocheck</b>: geen wijziging van de verstekwaarde in ckgedit;  <b>dokuwiki</b>:  gebruik de Dokuwiki taal schrijfrichting;  <b>ltr</b>: Links-naar-rechts; <b>rtl</b>: Rechts-naar-links.';
-$lang['scayt_auto']            = 'Activeer automatisch de SCAYT spellingcontrole. Verstekwaarde is "on". Om uit te schakelen selecteer "off"';
-$lang['scayt_lang']            = 'Activeer de SCAYT verstek taalkode.';
-$lang['smiley_hack']           = 'Reset de URL voor FCKeditor\'s smilies bij verhuis naar nieuwe server. Dit wordt pagina per pagina uitgevoerd bij het openen en bewaren van de pagina.  Deze optie zou normaal uit moeten staan.';
+$lang['nix_style']             = 'Voor Windows Servers (Vista en later).  Deze parameter maakt het mogelijk om data\media te benaderen met ckgedit\ckeditor\userfiles, indien links naar media en bestand succesvol aangemaakt zijn in userfiles';
+$lang['no_symlinks']           = 'Deactiveert automatische creatie van symbolische links in ckgedit/userfiles. Deze optie moet uitgeschakeld worden tijdens updaten.';
+$lang['direction']             = 'Zet de schrijf- en lees richting van de taal in CKeditor:  <b>nocheck</b>: geen wijziging van de standaard schrijfrichting in ckgedit;  <b>dokuwiki</b>:  gebruik de Dokuwiki taal schrijfrichting;  <b>ltr</b>: Links-naar-rechts; <b>rtl</b>: Rechts-naar-links.';
+$lang['scayt_auto']            = 'Stelt in of de SCAYT spellingcontrole, tijdens-het-typen, al actief is bij openen van het bewerkvenster. Standaard op <code>off</code>; de gebruiker kan de spellingscontrole inschakelen per pagina. Om de Scayt spellingscontrole compleet te verwijderen, kies <code>disable</code>. (Zie voor meer achtergrond <a href="https://www.dokuwiki.org/plugin:ckgedit:configuration#scayt_auto">ckgedit:configuration#scayt_auto</a>") ';
+$lang['scayt_lang']            = 'Activeer de SCAYT standaard taalcode.';
+$lang['smiley_hack']           = 'Reset de URL voor FCKeditor\'s smilies bij verhuizing naar nieuwe server. Dit wordt pagina per pagina uitgevoerd bij het openen en bewaren van de pagina.  Deze optie zou normaal uit moeten staan.';
 $lang['complex_tables']        = 'Gebruik het complex tables algorithm, in tegenstelling tot het standaard ontleding algoritme. Dit zou een beter resultaat geven wanneer complexe rowspans en colspans gemengd worden.';
-$lang['duplicate_notes']       = 'Zet deze optie op true (aanvinken) wanneer gebruikers meerdere voetnotas maken met dezelfde voettekst; deze optie is noodzakelijk om te voorkomen dat de voetnotas corrupt geraken.';
-$lang['winstyle']              = 'Gebruik een direct pad naar de mediamap in plaats van fckeditor/userfiles. Hiervoor moet fckeditor/userfiles/.htaccess.security worden gekopieerd naar data/media en worden hernoemd naar ".htaccess"';
+$lang['duplicate_notes']       = 'Zet deze optie op true (aanvinken) wanneer gebruikers meerdere voetnoten maken met dezelfde voettekst; deze optie is noodzakelijk om te voorkomen dat de voetnoten corrupt raken.';
+$lang['winstyle']              = 'Gebruik een direct pad naar de mediamap in plaats van fckeditor/userfiles. Deze functie kopieert <br />fckeditor/userfiles/.htaccess.security  naar data/media/.htaccess. Zo niet, dan moet dit handmatig gedaan worden.';
 $lang['other_lang']            = 'De standaardtaal voor de CKGEditor is de taal zoals ingesteld voor de browser. Je kunt echter een andere taal kiezen, onafhankelijk van de taal van het Dokuwiki-interface.';
 $lang['dw_priority']           = 'Maak DokuWiki editor de standaard editor';
 $lang['preload_ckeditorjs']    = 'Laad het javascript van de CKGeditor van tevoren om in het vervolg het laden van de editor te versnellen.';
@@ -38,3 +39,13 @@ $lang['font_options']          = 'Lettertype opties verwijderen';
 $lang['color_options']         = 'Kleur opties verwijderen';
 $lang['alt_toolbar']           = 'Functies om de CKEditor tool te verwijderen';
 $lang['mfiles']                = 'mfile-ondersteuning aanzetten';
+$lang['extra_plugins']         = 'Kommagescheiden lijst van aanvullende CKeditor plugins die aan de werkbalk toegevoegd worden. Zie de <a href=\'https://www.dokuwiki.org/plugin:ckgedit:configuration#extra_plugins\'>Configuration Page</a> van de ckgedit plugin voor details';
+$lang['dw_users']              = 'Groepsnaam van de gebruikers voor wie de standaard Dokuwiki-bewerkvenster weergegeven wordt, als <b>dw_priority</b> wordt ingeschakeld. Als er geen groepsnaam wordt opgegeven, dan krijgen alle gebruikers het originele DokuWiki-bewerkvenster als de optie <b>dw_priority</b> wordt ingeschakeld.';
+$lang['allow_ckg_filebrowser'] = 'Selecteer welke bestandsbrowser(s) gebruikers kunnen gebruiken';
+$lang['default_ckg_filebrowser'] = 'Selecteer welke bestandsbrowser standaard wordt gebruikt. Dit wordt overschreven als de geselecteerde bestandsbrowser niet is toegestaan.';
+$lang['captcha_auth']          = 'ACL-niveau waarop de captcha wordt uitgeschakeld als de captcha plugin\'s <code>forusers</code> is ingeschakeld. De standaardwaarde is <code>ACL_CREATE</code>, dit betekent dat elke gebruiker met <code>ACL_EDIT</code> of lager een captcha krijgt, en met <code>ACL_CREATE</code> of hoger niet.';
+$lang['htmlblock_ok']          = 'Als <code>HTML_BLOCK</code>-blokken worden gebruikt, dan moet of deze instelling of DokuWiki\'s <code>htmlok</code> optie worden ingeschakeld. Deze instelling geeft niet hetzelfde veiligheidsrisico als <code>htmlok</code>.  Desalniettemin moet deze instelling alleen gebruikt worden in een vertrouwde gebruikersomgeving, en niet in een open wiki.';
+$lang['dblclk']                = 'Kies <code>uit</code> om de dubbelklik-functie uit te schakelen die de gebruiker alinea\'s laat bewerken in het standaard DokuWiki\'s bewerkvenster (zie:  <a href=\'https://www.dokuwiki.org/plugin:ckgedit#direct_access_to_dokuwiki_editor\'>direct_access_to_dokuwiki_editor</a>)';
+$lang['preserve_enc']          = 'Behoud url-encodering in url\'s als DokuWiki\'s deaccent optie actief is';
+$lang['gui']                   = 'Kies een CKEditor GUI';
+$lang['rel_links']             = 'Activateer ondersteuning voor relatieve interne verwijzingen en afbeeldingsverwijzingen';
