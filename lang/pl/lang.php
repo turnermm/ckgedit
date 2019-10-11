2250429a5cb17475807aaf3a991eb8eaaf5b5442
DokuWiki Translation Tool <admin@dokuwiki.org>
2019-10-11 15:29:55 +0200

    translation update (#496)

---
+++ lang/pl/lang.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['btn_dw_edit']           = 'Edycja DW';
+$lang['dw_btn_fck_preview']    = 'Podgląd CKG';
+$lang['dw_btn_lang']           = 'Język';
+$lang['title_dw_delete']       = 'Usuń stronę';
+$lang['title_dw_edit']         = 'Zapisz pracę, Wyjdź, i Przełącz do natywnego edytora DokuWiki';
+$lang['dw_btn_revert']         = 'Przywróć';
+$lang['title_dw_revert']       = 'Przywróć poprzednią kopię zapasową';
+$lang['title_dw_lang']         = 'Wybierz język dla sprawdzania pisowni';
+$lang['title_dw_cancel']       = 'Opuść edytor';
+$lang['btn_fck_edit']          = 'Edycja CKG';
+$lang['confirm_delete']        = 'Jesteś pewien że chcesz usunąć tę stronę?';
+$lang['confirm_preview']       = 'Każda niezapisana praca zostanie utracona.';
+$lang['editor_height']         = 'Wysokość edytora';
+$lang['dw_btn_backup']         = 'Kopia zapasowa';
+$lang['btn_draft']             = 'Pobierz wersję roboczą';
+$lang['btn_exit_draft']        = 'Wyjdź z wersji roboczej';
+$lang['title_exit_draft']      = 'Wróć do bieżącego dokumentu';
+$lang['whats_this']            = 'co to jest?';
+$lang['minor_changes']         = 'Drobne zmiany';
+$lang['discard_edits']         = 'Aby zapisać zmiany kliknij OK; aby anulować kliknij Cancel';
+$lang['dw_btn_styling']        = 'Edycja czcionek';
+$lang['js']['on']              = 'wlączone';
+$lang['js']['off']             = 'wyłączone';
+$lang['uprofile_title']        = 'Wybierz swój domyślny edytor';
+$lang['dblclk_reminder']       = '<b>Przypomnienie</b>';
