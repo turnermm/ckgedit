b99e86c8febd0c46876da2c39e194fb5e93b9fe8
Florian Aigner <info@faigner.de>
2019-12-01 17:35:19 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -3,7 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author w.-g.esders <esders@esders.eu>
+ * @author Florian Aigner <info@faigner.de>
  * @author WhitesnakeS04 <whitesnakess04@gmail.com>
  * @author Sebastian Engel <mail@engel-s.de>
  * @author Joerg Maeder <member@toolcase.org>
@@ -15,16 +15,15 @@
  * @author Marco Hofmann <xenadmin@meinekleinefarm.net>
  * @author hans-juergen.schuemmer <hans-juergen.schuemmer@schuette.de>
  * @author Jens Weber <skynetworks@web.de>
- * @author Christoph Ziehr <info@einsatzleiterwiki.de>
  */
 $lang['btn_dw_edit']           = 'DW Edit';
 $lang['dw_btn_fck_preview']    = 'CKG Vorschau';
 $lang['dw_btn_lang']           = 'Sprache';
-$lang['title_dw_delete']       = 'Seite löschen';
+$lang['title_dw_delete']       = 'Seite l&ouml;schen';
 $lang['title_dw_edit']         = 'Arbeit speichern, schließen und zum ursprünglichen DokuWiki Editor wechseln';
-$lang['dw_btn_revert']         = 'Rückgängig';
-$lang['title_dw_revert']       = 'Zurück zur Backup-Version';
-$lang['title_dw_lang']         = 'Sprache für Rechtschreibkontrolle wählen';
+$lang['dw_btn_revert']         = 'R&uuml;ckg&auml;ngig';
+$lang['title_dw_revert']       = 'Zur&uuml;ck zur Backup-Version';
+$lang['title_dw_lang']         = 'Sprache f&uuml;r Rechtschreibkontrolle w&auml;hlen';
 $lang['title_dw_cancel']       = 'Editor schließen';
 $lang['btn_fck_edit']          = 'CKG Editor';
 $lang['confirm_delete']        = 'Sind Sie sicher, dass Sie diese Seite löschen wollen?';
@@ -34,61 +33,49 @@ $lang['editor_height_title']   = 'Editor neuskalieren beim Neuladen';
 $lang['dw_btn_backup']         = 'Backup';
 $lang['title_dw_backup']       = 'Backup-Editor-Fenster & Renew-Zugangssperre';
 $lang['backup_empty']          = 'Es ist kein Backup vorhanden. Wollen Sie fortfahren?';
-$lang['btn_draft']             = 'Entwurf öffnen';
+$lang['btn_draft']             = 'Entwurf &ouml;ffnen';
 $lang['title_draft']           = 'Entwurf anzeigen und bearbeiten';
 $lang['btn_exit_draft']        = 'Entwurf schließen';
 $lang['title_exit_draft']      = 'Zurück zum aktuellen Dokument.';
-$lang['draft_msg']             = 'Für dieses Dokument besteht eine Entwurfsversion. Der Knopf \'Entwurf\' schaltet zwischen diesem Dokument und dem Entwurf hin und her. Es können beide Dokumente bearbeitet oder gesichert werden.';
+$lang['draft_msg']             = 'F&uuml;r dieses Dokument besteht eine Entwurfsversion. Der Knopf \'Entwurf\' schaltet zwischen diesem Dokument und dem Entwurf hin und her. Es k&ouml;nnen beide Dokumente bearbeitet oder gesichert werden.';
 $lang['whats_this']            = 'Was ist das?';
 $lang['complex_tables']        = 'Aktiviere komplexe Tabellen';
 $lang['minor_changes']         = 'Kleine Änderungen';
-$lang['discard_edits']         = 'Um Änderungen zu speichern klicke OK; um abzubrechen klicke Abbrechen.';
+$lang['discard_edits']         = 'Um Änderungen zu speichern klicke OK; um abzubrechen klicke Cancel.';
 $lang['dw_btn_styling']        = 'Schriftart bearbeiten';
 $lang['title_styling']         = 'Öffne mit Schriftart als markup';
 $lang['js']['font_err_1']      = 'Formatierungen können nicht in Links genutzt werden. Klicken Sie auf OK, um die folgende Korrektur zu akzeptieren:';
-$lang['js']['font_err_2']      = 'Um zu Überarbeiten, geben Sie den vollständigen Dokuwiki-Link  unten, einschließlich Klammern ein. Um zum Editor zurückzukehren, klicken Sie auf Abbrechen.';
+$lang['js']['font_err_2']      = 'Um zu überarbeiten, geben Sie den vollständigen Dokuwik- Link  unten, einschließlich Klammern ein. Um zum Editor zurückzukehren, klicken Sie auf Abbrechen.';
 $lang['js']['font_err_throw']  = 'Zeichenformatierungs-Fehler';
-$lang['js']['dwp_save_err']    = 'Speichern nicht möglich für:';
+$lang['js']['dwp_save_err']    = 'Speichern nicht m&ouml;glich f&uuml;r:';
 $lang['js']['dwp_updated']     = 'Editor Priorität aktualisiert auf:';
 $lang['js']['dwp_not_sel']     = 'Editor-Priorität nicht ausgewählt: Es wird durch die Konfiguration der dw_users-Gruppe bestimmt';
 $lang['js']['mediamgr_notice'] = 'Verwenden Sie den Link Dialog um Links einzufügen';
 $lang['js']['font_conflict']   = 'Der Parser fand einen oder mehrere Link-Font-Konflikte. Wenn Sie auf OK klicken, können Sie zum Editor zurückkehren und die Schriftstile entfernen. Weitere Informationen finden Sie unter: https://www.dokuwiki.org/plugin:ckgedit:font_styling#conflicts_with_dokuwiki_links';
-$lang['mediamgr_imgonly'] = 'Benutzen Sie den Bild-Dialog nur für Bilder. Verwenden Sie den Link-Dialog für andere Dateitypen. Die betroffene Datei ist:';
+$lang['js']['ckg_paste_restart'] = 'Beim Neuladen des Editors wird die zus&auml;tzliche Unterst&uuml;tzung für das Einfügen eingeschaltet.';
+$lang['js']['on']              = 'ein';
+$lang['js']['off']             = 'aus';
+$lang['js']['broken_image_1']  = 'Nutze das imagePaste plugin mit dem Dokuwiki Mediamanager oder probiere das MS Word einfügen tool.';
+$lang['js']['broken_image_2']  = 'Ctrl-v von Bildern mit Rohdaten über 2,5 MB kann sich aufhängen. Dieses Bild hat:  ';
 $lang['mediamgr_imgonly']      = 'Laden Sie nur Bilder hoch, wenn Sie den Bilddialog verwenden: ';
-$lang['uprofile_title']        = 'Wählen Sie ihren Standard Editor';
+$lang['uprofile_title']        = 'W&auml;hlen Sie ihren Standard Editor';
 $lang['btn_val_dw_fb']         = 'DW Dateibrowser';
 $lang['btn_val_ckg_fb']        = 'CKG Dateibrowser';
 $lang['btn_title_dw_fb']       = 'Speichern und Editor schließen und zum DW Dateibrowser wechseln';
 $lang['btn_title_ckg_fb']      = 'Speichern und Editor schließen und zum CKG Dateibrowser wechseln';
-$lang['formatdel']             = 'Markup wurde aus Header(n) entfernt: Von DokuWiki nicht unterstützt';
-$lang['fontdel']               = 'Markup wurde aus Link(s) entfernt: Von DokuWiki nicht unterstützt';
+$lang['formatdel']             = 'Markup wurde aus Header(n) entfernt: Von DokuWiki nicht unterst&uuml;tzt';
+$lang['fontdel']               = 'Markup wurde aus Link(s) entfernt: Von DokuWiki nicht unterst&uuml;tzt';
 $lang['ws_cantcopy']           = 'Für das winstyle Setup: kann nicht kopiert werden nach';
 $lang['ws_copiedhtaccess']     = 'Für das winstyle Setup wurde das Security-aktivierte .htaccess nach data/media kopiert. Siehe ckgedit/fckeditor/userfiles/.htacess.security ';
-$lang['userfiles_perm']        = 'Bitte überprüfen Sie die Berechtigungen; ckgedit hat keinen Zugriff';
+$lang['userfiles_perm']        = 'Bitte &uuml;berprüfen Sie die Berechtigungen; ckgedit hat keinen Zugriff';
 $lang['sym_not created_1']     = 'Kann nicht erstellt werden ';
 $lang['sym_not created_2']     = 'Kann keine Symlinks für den Dateibrowser erstellen: kein Zugriff: ';
 $lang['sym_not created_3']     = 'Es gab einen Fehler beim Versuch symbolische Links zu erstellen in';
 $lang['syms_created']          = 'Die folgenden Links wurden im Verzeichnis userfiles angelegt: ';
-$lang['dblclk']                = '<b>Neues Feature:</b> Doppelklick im Browserfenster um den DW Editor zum Bearbeiten eines Abschnitts an der Kursorposition zu öffnen. <b>Siehe: </b><a href=\'https://www.dokuwiki.org/plugin:ckgedit#direct_access_to_dokuwiki_editor\'>direct_access_to_dokuwiki_editor</a>. ';
+$lang['dblclk']                = '<b>Neues Feature:</b> Doppelklick im Browserfenster um den DW Editor zum Bearbeiten eines Abschnitts an der Kursorposition zu &ouml;ffnen. <b>Siehe: </b><a href=\'https://www.dokuwiki.org/plugin:ckgedit#direct_access_to_dokuwiki_editor\'>direct_access_to_dokuwiki_editor</a>. ';
 $lang['dblclk_reminder']       = '<b>Erinnerung</b>';
 $lang['ckg_img_paste']         = 'Zusätzliche Unterstützung beim Einfügen von Bildern aus der Zwischenablage';
-$lang['ckg_img_paste_title']   = 'Aus um doppeltes Einfügen zu verhindern, oder Ein, um Einfügen zu erlauben';
-$lang['js']['ckg_paste_restart'] = 'Beim Neuladen des Editors wird die zusätzliche Unterstützung für das Einfügen eingeschaltet.';
+$lang['ckg_img_paste_title']   = 'Aus um doppeltes Einf&uuml;gen zu verhindern, oder Ein, um Einfügen zu erlauben';
 $lang['on']                    = 'Ein';
 $lang['off']                   = 'Aus';
-$lang['js']['on']              = 'ein';
-$lang['js']['off']             = 'aus';
-$lang['broken_image']          = 'Benutze das ImagePaste Plugin mit dem Mediamanager oder versuche das MS Word Tool. STRG + V mit Bildern > 2,5M funktioniert evtl. nicht. Dieses Bild hat folgende Größe:';
-$lang['js']['broken_image_1']  = 'benutze das ImagePaste Plugin mit dem Dokuwiki Mediamanager oder versuche das MS Word Einfügen Tool';
-$lang['js']['broken_image_2']  = 'Bilder grösser 2,5M mit STRG + V einfügen funktioniert nicht. Dieses Bild hat folgende Größe:';
-$lang['menu'] = 'CKEditor CSS Tool'; 
-$lang['default_stylesheet']  = 'Ein Stylesheet für das aktuelle Template anlegen';
-$lang['alt_stylesheet']  = 'Ein Stylesheet für ein anderes Template anlegen'; 
-$lang['style_sheet'] = 'Stylesheet anlegen';
-$lang['style_sheet_msg'] = 'Ein Stylesheet für das aktuelle Template anlegen:';
-$lang['alt_style_sheet_msg'] = 'Stylesheet anlegen für:';
-$lang['checkbox'] = 'Nach ckeditor/css kopieren';
-$lang['stylesheet_oinfo'] = 'Info';
-$lang['stylesheet_cinfo'] = 'Info schließen';
-$lang['js']['stylesheet_oinfo'] = $lang['stylesheet_oinfo'];
-$lang['js']['stylesheet_cinfo'] = $lang['stylesheet_cinfo'];
+$lang['broken_image']          = 'Nutze das imagePaste plugin mit dem Dokuwiki Mediamanager oder probiere das MS Word einfügen tool. Ctrl-v von Bildern mit Rohdaten über 2,5 MB kann sich aufhängen. Dieses Bild hat:  ';
