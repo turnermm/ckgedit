89ca4942de82378faa71acdbff708150c42a9ce2
DokuWiki Translation Tool <admin@dokuwiki.org>
2020-06-08 15:53:39 +0200

    translation update (#551)
    Co-authored-by: 切 <3537509263@qq.com>

---
+++ lang/zh/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 切 <3537509263@qq.com>
+ */
+$lang['openfb']                = 'Open the file to browse. This gives the user access to the entire directory structure, whether the user has permissions or not. The ACL still applies to uploads.';
+$lang['dw_edit_display']       = 'Control which users can access the "DW Edit" button. Choice: "All" for all users; "Administrator" applies only to administrators and managers; "None" for anyone. The default is "All".';
+$lang['smiley_as_text']        = 'Display emoticons as text in CKeditor (It will still appear as an image in the browser.)';
