9e278266d010110d09bc5d7b5675c8d3164b1927
Myron Turner <turnermm02@shaw.ca>
2020-07-29 10:46:05 -0500

    synced mediamgr.js with hogfather-merged

--- scripts/mediamgr.js
+++ scripts/mediamgr.js
@@ -1,8 +1,10 @@
       
       if (opener != null && opener.CKEDITOR !== undefined) {
             window.onload = function () {
-                jQuery( document ).ready(function() {
+                var _action = '?ns=&edid=wiki__text&onselect=ckg_edit_mediaman_insert&ckg_media=img&CKEditor=wiki__text&CKEditorFuncNum=1&langCode=en';
+                
                     if ((location.search.split("ckg_media=")[1]||"").split("&")[0] == "link") {
+                	_action = '?ns=&edid=wiki__text&onselect=ckg_edit_mediaman_insertlink&ckg_media=link&CKEditor=wiki__text&CKEditorFuncNum=1&langCode=en';
                         jQuery(".select").on("click", function(event) {
                             var $link, id;
 
@@ -18,21 +20,31 @@
                         ckg_nonimage_overlay();
                     }
 
-					 if(jQuery("form#dw__mediasearch button").length){				
-						 jQuery("form#dw__mediasearch").hide()				 
-					 }
-                });
+                /* update search form action */
+                 var valu = jQuery("#dw__mediasearch  input[type='text").val();
+                 if(valu == ""){
+                     var url = jQuery("form#dw__mediasearch").attr('action'); 
+                      jQuery("form#dw__mediasearch").attr('action', url+_action); 
+                  };
 
                 jQuery(document).ajaxComplete(function() {
                     ckg_nonimage_overlay();
+                 
+                    jQuery("form[action]").each(function(index, val){
+                        var url = jQuery(this).attr('action'); 
+                        url = url + _action;              
+                        jQuery(this).attr('action',url);                       
+                   });
                 });
             };
             
+            if(!opener.oDokuWiki_FCKEditorInstanceInLinkDialog)  {
            jQuery( ".odd, .even" ).each( function( index, element ){
                 if(!this.title.match(/\.(jpg|jpeg|png|tiff?|gif)$/)){
                     jQuery( this ).html(LANG.plugins.ckgedit.mediamgr_notice+": <b>" + this.title  +"</b>");
                 }
             });
+            }
         
         }
 
