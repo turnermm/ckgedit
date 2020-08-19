7c858c6440d47fc1e3d2b2acda2a68b26eb7dfef
Myron Turner <turnermm02@shaw.ca>
2020-08-19 09:04:41 -0500

    Synced with hogfather-merged after hogfather-merged was merged with iwiki-link

--- action/save.php
+++ action/save.php
@@ -199,13 +199,8 @@ $TEXT = preg_replace_callback("#<code\s+(\w+)>.*?(\[enable_line_numbers.*?\])\s*
                         return $matches[0];
                     }
                     if(preg_match('/[\w\.]+\s*>/',$matches[0])) {
-                        // exclude dokuwiki's wiki links but first remove url display text if needed
-                        list($type,$display_url,$rest) = explode('|', $matches[0]);
-                        if(!isset($display_url)) {                       
                            return $matches[0]; 
                         }
-                        return $type . '|]]';
-                    }
 	 	             if(preg_match('/([\w\.\-]+@[\w\.\-]+\.\w{2,3})\?.*?\|\1/i',$matches[0])) {
                              return $matches[0];
                      }
