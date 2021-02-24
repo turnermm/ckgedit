7392023d3e2cf7dc93ee5a5eabc90946aa7ca431
Hérisson grognon <dodoperso@laposte.net>
2021-02-24 14:20:47 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Hérisson grognon <dodoperso@laposte.net>
  * @author salabdou <salabdou@gmail.com>
  * @author TJ619 <rockandrollfever0@gmail.com>
  * @author Laurent Marquet <laurent.marquet@laposte.net>
@@ -15,24 +16,24 @@ $lang['dw_btn_fck_preview']    = 'Prévisualisation CKG';
 $lang['dw_btn_lang']           = 'Langue';
 $lang['title_dw_delete']       = 'Supprimer la page';
 $lang['title_dw_edit']         = 'Sauvegarder le travail, quitter et retourner sur l\'éditeur natif DokuWiki';
-$lang['dw_btn_revert']         = 'Restaurer dernière sauvegarde';
+$lang['dw_btn_revert']         = 'Restaurer la dernière sauvegarde';
 $lang['title_dw_revert']       = 'Revenir à la dernière sauvegarde';
 $lang['title_dw_lang']         = 'Sélectionner la langue pour la vérification orthographique';
 $lang['title_dw_cancel']       = 'Quitter l\'éditeur';
 $lang['btn_fck_edit']          = 'Éditeur CKG';
 $lang['confirm_delete']        = 'Êtes-vous sûr de vouloir supprimer cette page ?';
-$lang['confirm_preview']       = 'Tout travail non sauvegardé sera perdu';
+$lang['confirm_preview']       = 'Tout travail non sauvegardé sera perdu.';
 $lang['editor_height']         = 'Hauteur de l\'éditeur : ';
-$lang['editor_height_title']   = 'L\'éditeur se redimensionne au rechargement';
+$lang['editor_height_title']   = 'L\'éditeur se redimensionne au rechargement.';
 $lang['dw_btn_backup']         = 'Sauvegarde';
-$lang['title_dw_backup']       = 'Sauve la fenêtre de l\'éditeur & renouvelle le verrou';
+$lang['title_dw_backup']       = 'Sauvegarde la fenêtre de l\'éditeur et renouvelle le verrou';
 $lang['backup_empty']          = 'Le tampon de sauvegarde est vide. Voulez-vous continuer ?';
-$lang['btn_draft']             = 'Obtenir Brouillon';
-$lang['title_draft']           = 'Voir, Montrer, Éditer Brouillon';
-$lang['btn_exit_draft']        = 'Quitter Brouillon';
+$lang['btn_draft']             = 'Obtenir le brouillon';
+$lang['title_draft']           = 'Voir, Afficher, Éditer le brouillon';
+$lang['btn_exit_draft']        = 'Quitter le brouillon';
 $lang['title_exit_draft']      = 'Retourner au document courant';
-$lang['draft_msg']             = 'Ce document a un brouillon. Le bouton Obtenir/Quitter Brouillon bascule entre ce document et le brouillon. Vous pouvez éditer et sauver l\'un ou l\'autre.';
-$lang['whats_this']            = 'Qu\'est-ce ?';
+$lang['draft_msg']             = 'Ce document a un brouillon. Le bouton Obtenir/Quitter le brouillon bascule entre ce document et le brouillon. Vous pouvez éditer et sauver l\'un ou l\'autre.';
+$lang['whats_this']            = 'Qu\'est-ce que c\'est ?';
 $lang['complex_tables']        = 'Autoriser les tableaux complexes';
 $lang['minor_changes']         = 'Changements mineurs';
 $lang['discard_edits']         = 'Pour sauver vos modifcations, cliquez sur OK, sinon cliquez sur Annuler.';
@@ -57,6 +58,7 @@ $lang['btn_title_dw_fb']       = 'Sauver, fermer l\'éditeur et passer au gestio
 $lang['btn_title_ckg_fb']      = 'Sauver, fermer l\'éditeur et passer au gestionnaire de fichiers CKG';
 $lang['formatdel']             = 'Balisage supprimé des entêtes: indisponible dans DokuWiki';
 $lang['fontdel']               = 'Polices différentes supprimées des liens: indisponible dans DokuWiki';
+$lang['ckgcke_conflict']       = 'Une copie de ckgedit a été détectée. L\'un des Ckgedit et ckgedit doit être désactivé.';
 $lang['ws_cantcopy']           = 'Pour la configuration de Winstyle: impossible de copier vers';
 $lang['ws_copiedhtaccess']     = 'Pour l’installation de Winstyle, copiez le fichier .htaccess avec sécurité activée dans data/media
 Voir ckgedit/fckeditor/userfiles/.htaccess.security';
