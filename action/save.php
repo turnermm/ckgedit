e720e00801dc4b824e0bc51bbec469c41a9a6df3
Myron Turner <turnermm02@shaw.ca>
2021-06-12 13:52:47 -0500

    Removes extra spaces at start and end of font tags

--- action/save.php
+++ action/save.php
@@ -289,6 +289,11 @@ Removed newlines and spaces from beginnings and ends of text enclosed by font ta
          ),
          $TEXT
        );
+       
+       $TEXT = preg_replace("/\s+<font/ms"," <font", $TEXT);
+       $TEXT = preg_replace("/<\/font>\s+/ms","</font> ", $TEXT);
+       $TEXT = preg_replace('/\s{2,}<font/ms',' <font',$TEXT);
+       $TEXT = preg_replace('/font>{2,}/ms',' font> ',$TEXT);
        $TEXT = preg_replace('/__QUOTE__/ms',">",$TEXT);
        $TEXT = preg_replace('/[\t\x20]+$/ms',"",$TEXT);
        $TEXT = preg_replace('/\n{4,}/ms',"\n\n",$TEXT);
