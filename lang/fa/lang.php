ab74eaec0dfcb18d396e43f1a38f841b01651872
Nima Hamidian <nima.hamidian@gmail.com>
2021-06-23 22:05:18 +0200

    translation update

---
+++ lang/fa/lang.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Nima Hamidian <nima.hamidian@gmail.com>
+ */
+$lang['btn_dw_edit']           = 'ویرایش دوکوویکی';
+$lang['dw_btn_fck_preview']    = 'پیش‌نمایش CKG';
+$lang['dw_btn_lang']           = 'زبان';
+$lang['title_dw_delete']       = 'حذف صفحه';
+$lang['title_dw_edit']         = 'ذخیره کار، خروج، و انتقال به ویرایشگر اصلی دوکوویکی';
+$lang['dw_btn_revert']         = 'برگشت';
+$lang['title_dw_revert']       = 'برگشت به بکاپ قبلی';
+$lang['title_dw_lang']         = 'انتخاب زبان برای بررسی هِجی';
+$lang['title_dw_cancel']       = 'خروج از ویرایشگر';
+$lang['btn_fck_edit']          = 'ویرایش CKG';
+$lang['confirm_delete']        = 'آیا مطمئنید می‌خواهید این صفحه را حذف کنید؟';
+$lang['confirm_preview']       = 'هر کار که ذخیره نشده باشد از دست خواهد رفت.';
+$lang['editor_height']         = 'ارتفاع ویرایشگر';
+$lang['editor_height_title']   = 'اندازه ویرایشگر هنگام بارگذاری مجدد بِروز خواهد شد';
+$lang['dw_btn_backup']         = 'بکاپ';
+$lang['title_dw_backup']       = 'بکاپ‌گرفتن از پنجره ویرایشگر و تجدید قفل';
+$lang['backup_empty']          = 'حافظه بکاپ ظاهراً خالی است. آیا می‌خواهید ادامه دهید؟';
+$lang['btn_draft']             = 'دریافت پیش‌نویس';
+$lang['title_draft']           = 'دیدن، نمایش، ویرایش پیش‌نویس';
+$lang['btn_exit_draft']        = 'خروج از پیش‌نویس';
+$lang['title_exit_draft']      = 'بازگشت به سند فعلی';
+$lang['draft_msg']             = 'این سند یک فایل پیش‌نویس دارد. دکمه پیش‌نویس بین این سند و پیش‌نویس جابجا می‌کند. شما می‌توانید هر یک از این دو را ویرایش یا ذخیره کنید.';
+$lang['whats_this']            = 'این چیست؟';
+$lang['complex_tables']        = 'فعالسازی امکان جداول پیچیده';
+$lang['minor_changes']         = 'تغییرات جزئی';
+$lang['discard_edits']         = 'برای ذخیره ویرایش‌هایتان روی بلی کلیک کنید؛ برای حذف روی لغو کلیک کنید';
+$lang['dw_btn_styling']        = 'ویرایش فونت‌ها';
+$lang['title_styling']         = 'گشودن با نوع فونتهای نشان‌داده‌شده به عنوان مارکاپ';
+$lang['js']['font_err_1']      = 'نوه فونت نمی‌تواند در این پیوندها مشمول شود. برای پذیرفتن تصحیح زیر روی بلی کلیک کنید:';
+$lang['js']['font_err_2']      = 'جهت تجدیدنظر، مارکاپ کامل پیوند دوکوویکی را شامل براکت‌ها در پایین وارد کنید.
+برای بازگشت به ویرایشگر روی لغو کلیک کنید';
+$lang['js']['font_err_throw']  = 'خطای فرمت‌دهی فونت';
+$lang['js']['dwp_save_err']    = 'عدم امکان ذخیره تغییرات در:';
+$lang['js']['dwp_updated']     = 'اولویت ویرایشگر بِروزسازی شد به:';
+$lang['js']['dwp_not_sel']     = 'اولویت ویرایشگر انتخاب نشد: این توسط تنظیمات config گروه dw_users تعیین می‌شود';
+$lang['js']['mediamgr_notice'] = 'از دیالوگ پیوند برای واردکردن استفاده کنید';
+$lang['js']['font_conflict']   = 'Parser دچار یک یا تعداد بیشتر مشکلات فونتِ پیوند شد. با کلیک روی بلی شما می‌توانید به ویرایشگر برگردید و نوعهای فونت را بردارید.برای اطلاعات بیشتر نگاه کنید به: https://www.dokuwiki.org/plugin:ckgedit:font_styling#conflicts_with_dokuwiki_links';
+$lang['js']['ckg_paste_restart'] = 'هنگام بارگذاری مجدد ویرایشگر، پشتیبانی اضافی paste به این صورت در می‌آید:';
+$lang['js']['on']              = 'روشن';
+$lang['js']['off']             = 'خاموش';
+$lang['js']['broken_image_1']  = 'از پلاگین ImagePaste به همراه مدیریت رسانه‌ای دوکوویکی استفاده شود یا ابزار paste نرم‌افزار MS Word امتحان شود.';
+$lang['js']['broken_image_2']  = 'Ctrl-v کردن تصاویر به همراه داده‌های خام با حجم بیش از ۲.۵ مگابایت می‌تواند هنگ کند. این تصویر عبارتست از:';
+$lang['mediamgr_imgonly']      = 'از دیالوگ تصاویر فقط برای تصواویر استفاده شود، برای سایر رسانه‌ها از دیالوگ پیوند. این فایل عبارتست از:';
+$lang['uprofile_title']        = 'ویرایشگر پیش‌فرض خود را انتخاب کنید';
+$lang['btn_val_dw_fb']         = 'مرورگر فایل دوکوویکی';
+$lang['btn_val_ckg_fb']        = 'مرورگر فایل CKG';
+$lang['btn_title_dw_fb']       = 'ذخیره و بستن ویرایشگر و انتقالبه مرورگر فایل دوکوویکی';
+$lang['btn_title_ckg_fb']      = 'ذخیره و بستن ویرایشگر و انتقالبه مرورگر فایل CKG';
+$lang['formatdel']             = 'مارکاپ از سرتیترها برداشته شد: توسط دوکوویکی پشتیبانی نمی‌شود';
+$lang['fontdel']               = 'مارکاپ فونت از پیوندها برداشته شد: توسط دوکوویکی پشتیبانی نمی‌شود';
+$lang['ckgcke_conflict']       = 'یک رونوشت از ckgedit شناسایی شد. یا ckgedit یا ckgedit باید غیرفعال شوند.';
+$lang['ws_cantcopy']           = 'برای نصب ویندوزمانند: نمی‌توان رونوشت کرد به:';
+$lang['ws_copiedhtaccess']     = 'برای نصب ویندوزمانند، فایل.htaccess  فعال‌شده از نظر امنیت به data/media کپی شد.
+نگاه کنید به ckgedit/fckeditor/userfiles/.htacess.security';
+$lang['userfiles_perm']        = 'لطفا اجازه‌های دسترسی را بررسی کنید؛ ckgedit نمی‌تواند دسترسی یابد';
+$lang['sym_not created_1']     = 'عدم امکان ایجاد';
+$lang['sym_not created_2']     = 'نمی‌تواند پیوندهای نمادین برای مرورگر فایل ایجاد کند: نمی‌تواند دسترسی یابد:';
+$lang['sym_not created_3']     = 'رخ‌دادن خطایی هنگام تلاش برای ایجاد پیوندهای نمادین در:';
+$lang['syms_created']          = 'پیوندهای زیر در پوشه userfiles ایجاد شدند:';
+$lang['dblclk']                = '<b>ویژگیهای جدید:</b> 
+روی پنجره مرورگر کلیک دوتایی کنید تا ویرایشگر دوکوویکی برای ویرایش بخشی در محل مکان‌نما باز شود. <b> نگاه کنید: </b><a href=\'https://www.dokuwiki.org/plugin:ckgedit#direct_access_to_dokuwiki_editor\'>direct_access_to_dokuwiki_editor</a>. ';
+$lang['dblclk_reminder']       = '<b>یادآور</b> ';
+$lang['ckg_img_paste']         = 'پشتیبانی اضافی  paste تصویر';
+$lang['ckg_img_paste_title']   = 'خاموش جهت پیشگیری از paste دوتایی، یا فعال‌کردن paste ها';
+$lang['on']                    = 'روشن';
+$lang['off']                   = 'خاموش';
+$lang['broken_image']          = 'از پلاگین ImagePaste به همراه مدیریت رسانه‌ای دوکوویکی استفاده شود یا ابزار Paste نرم‌افزار MS Word امتحان شود. داده‌های خام Ctrl-v تصاویر بزرگتر از ۲.۵ مگابایت می‌تواند هنگ کند. این تصویر عبارتست از:';
