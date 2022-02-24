a62a4361dce64cec359773d19011e9118c480a1e
Myron Turner <turnermm02@shaw.ca>
2022-02-23 19:08:23 -0600

    Removed some debugging in edit.php

--- action/edit.php
+++ action/edit.php
@@ -166,11 +166,6 @@ class action_plugin_ckgedit_edit extends DokuWiki_Action_Plugin {
   
         global $INFO;
         global $conf;
-        if($this->helper->has_plugin('imgpaste')) {
-            $imgpaste = plugin_load('action','imgpaste'); 
-            $ifn = $imgpaste->getConf('filename');
-            echo '<pre>' .print_r($ifn,1) . '</pre>';
-        }
 
         // we only change the edit behaviour
         if ($event->data != 'edit') {
