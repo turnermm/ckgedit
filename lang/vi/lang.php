823f8cbc46e2867acbfa1d544280deb3e17a85c7
Minh <phandinhminh@protonmail.ch>
2023-01-24 15:15:20 +0100

    translation update

---
+++ lang/vi/lang.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Minh <phandinhminh@protonmail.ch>
+ */
+$lang['btn_dw_edit']           = 'Chỉnh sửa DW';
+$lang['dw_btn_fck_preview']    = 'Xem trước CKG';
+$lang['dw_btn_lang']           = 'Ngôn ngữ';
+$lang['title_dw_delete']       = 'Xoá trang';
+$lang['title_dw_edit']         = 'Lưu Tác vụ, Thoát, và Đổi sang trình chỉnh sửa DokuWiki gốc';
+$lang['dw_btn_revert']         = 'Hoàn nguyên';
+$lang['title_dw_revert']       = 'Hoàn nguyên lại về bản sao lưu trước';
+$lang['title_dw_lang']         = 'Chọn ngôn ngữ cho việc kiểm tra chính tả';
+$lang['title_dw_cancel']       = 'Thoát khỏi trình chỉnh sửa';
+$lang['btn_fck_edit']          = 'Chỉnh sửa CKG';
+$lang['confirm_delete']        = 'Bạn có chắc muốn xoá trang này không?';
+$lang['confirm_preview']       = 'Mọi công việc chưa được sao lưu sẽ bị mất.';
+$lang['editor_height']         = 'Độ cao trình chỉnh sửa';
+$lang['editor_height_title']   = 'Trình chỉnh sửa đặt lại kích cỡ khi tải lại';
+$lang['dw_btn_backup']         = 'Sao lưu';
+$lang['title_dw_backup']       = 'Cửa sổ trình chỉnh sửa sao lưu & khoá Làm mới lại';
+$lang['backup_empty']          = 'Bộ đệm dự phòng sao lưu dường như trống. Bạn có muốn tiếp tục không?';
+$lang['btn_draft']             = 'Lấy bản nháp';
+$lang['title_draft']           = 'Xem, Hiện, Chỉnh sửa Nháp';
+$lang['btn_exit_draft']        = 'Thoát Nháp';
+$lang['title_exit_draft']      = 'Quay lại về tài liệu hiện thời';
+$lang['draft_msg']             = 'Tài liệu này có một bản file nháp. Nút nháp sẽ chuyển qua lại giữa bản tài liệu này và bản nháp. Bạn có thể chỉnh sửa và sao lưu một trong hai.';
+$lang['whats_this']            = 'đây là gì?';
+$lang['complex_tables']        = 'Cho phép các Bảng Phức tạp';
+$lang['minor_changes']         = 'Các thay đổi nhỏ';
+$lang['discard_edits']         = 'Để Lưu lại các bản chỉnh sửa của bạn nhấp OK; để Bỏ đi nhấp Bỏ';
+$lang['dw_btn_styling']        = 'Chỉnh sửa Font';
+$lang['title_styling']         = 'Mở với các kiểu font được hiện ra như Đánh dấu';
+$lang['js']['font_err_1']      = 'Thay đổi kiểu font không thể được thêm vào các liên kết. Nhấp OK để chấp nhận sửa đổi sau:';
+$lang['js']['font_err_2']      = 'Để xem lại, nhập Đánh dấu toàn bộ đường dẫn Dokuwiki bên dưới, bao gồm các ngoặc. Để quay lại về Trình chỉnh sửa nhấp Bỏ.';
+$lang['js']['font_err_throw']  = 'Lỗi định dạng phông chữ';
+$lang['js']['dwp_save_err']    = 'Không thể lưu các thay đổi tới:';
+$lang['js']['dwp_updated']     = 'Độ ưu tiên trình chỉnh sửa được cập nhật lên tới:';
+$lang['js']['dwp_not_sel']     = 'Độ ưu tiên trình chỉnh sửa chưa được chọn: nó sẽ được xác định bởi nhóm dw_users cài đặt cấu hình ';
+$lang['js']['mediamgr_notice'] = 'Sử dụng hộp thoại liên kết để chèn';
+$lang['js']['font_conflict']   = 'Trình phân tích cú pháp đã tìm thấy một hoặc nhiều xung đột đối với font liên kết. Bằng cách nhấp OK, bạn có thể quay lại trình chỉnh sửa và xóa các kiểu font. Để biết thêm thông tin xem: https://www.dokuwiki.org/plugin:ckgedit:font_styling#conflicts_with_dokuwiki_links';
+$lang['js']['ckg_paste_restart'] = 'Khi tải lại trình chỉnh sửa, hỗ trợ dán bổ sung sẽ được bật';
+$lang['js']['on']              = 'trên';
+$lang['js']['off']             = 'tắt';
+$lang['uprofile_title']        = 'Chọn trình chỉnh sửa mặc định';
+$lang['btn_val_dw_fb']         = 'Trình duyệt tệp DW';
+$lang['btn_val_ckg_fb']        = 'Trình duyệt tệp CKG';
+$lang['btn_title_dw_fb']       = 'Lưu và đóng trình chỉnh sửa và chuyển sang Trình duyệt tệp DW';
+$lang['btn_title_ckg_fb']      = 'Lưu và đóng trình chỉnh sửa và chuyển sang Trình duyệt tệp CKG';
+$lang['formatdel']             = 'Đánh dấu bị xóa khỏi (các) tiêu đề: không được Dokuwiki hỗ trợ';
+$lang['fontdel']               = 'Đánh dấu font chữ đã bị xóa khỏi (các) liên kết: không được Dokuwiki hỗ trợ';
+$lang['ckgcke_conflict']       = 'Một bản sao của ckgedit đã được phát hiện. Phải tắt ckgedit hoặc ckgedit đi.';
+$lang['ws_cantcopy']           = 'Đối với thiết lập winstyle: không thể sao chép tới';
+$lang['ws_copiedhtaccess']     = 'Đối với thiết lập winstyle, sao chép .htaccess đã được bật bảo mật vào dữ liệu/phương tiện
+Xem ckgedit/fckeditor/userfiles/.htacess.security';
+$lang['userfiles_perm']        = 'Vui lòng kiểm tra quyền; ckgedit không thể truy cập được';
+$lang['sym_not created_1']     = 'Không thể tạo';
+$lang['sym_not created_2']     = 'Không thể tạo symlink cho trình duyệt tệp:  không thể truy cập:';
+$lang['sym_not created_3']     = 'Đã có lỗi xảy ra khi cố gắng tạo liên kết tượng trưng symlink trong';
+$lang['syms_created']          = 'Các đường dẫn sau đây đã được tạo trong thư mục tệp người dùng userfile';
+$lang['dblclk_reminder']       = '<b>Nhắc nhở</b> ';
+$lang['ckg_img_paste']         = 'Bật hỗ trợ dán ảnh bổ sung';
+$lang['on']                    = 'trên';
+$lang['off']                   = 'tắt';
---
+++ lang/vi/settings.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Minh <phandinhminh@protonmail.ch>
+ */
+$lang['groups']                = 'Nhóm đã được cho phép tắt khoá hẹn giờ (không dùng nữa)';
+$lang['fck_preview']           = 'Nhóm xem trước FCK';
+$lang['guest_toolbar']         = 'Hiện thanh công cụ cho Khách';
+$lang['guest_media']           = 'Khách có thể dẫn đường link tới Các tệp Media';
+$lang['open_upload']           = 'Khách có thể tải lên';
+$lang['duplicate_notes']       = 'Đặt điều này thành đúng (true) nếu người dùng tạo nhiều chú thích cuối trang với cùng văn bản chú thích cuối trang; cần thiết để ngăn ghi chú bị hỏng.';
+$lang['dw_priority']           = 'Đặt trình chỉnh sửa Dokuwiki làm trình soạn thảo mặc định: không hoạt động trong các trại (farms)';
+$lang['preload_ckeditorjs']    = 'Tải trước javascript của ckeditor để tăng tốc độ tải trình chỉnh sửa sau này';
+$lang['nofont_styling']        = 'Hiển thị các kiểu font chữ trong trình chỉnh sửa dưới dạng tiện ích đánh dấu plugin. Xem trang plugin ckgedit tại Dokuwiki.org để biết thêm chi tiết.';
+$lang['font_options']          = 'Bỏ tuỳ chọn font.';
+$lang['color_options']         = 'Bỏ tuỳ chọn màu.';
+$lang['mfiles']                = 'Bật hỗ trợ tệp mfile';
+$lang['allow_ckg_filebrowser'] = 'Chọn (những) trình duyệt tệp/phương tiện mà người dùng có thể sử dụng';
+$lang['gui']                   = 'Chọn CKEditor GUI.';
+$lang['rel_links']             = 'Kích hoạt hỗ trợ cho các liên kết hình ảnh và nội bộ tương đối';
